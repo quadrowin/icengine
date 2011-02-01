34638f0d914f0a55992c0fe6c637a68c7c977e90
Goorus <Goorus@localhost>
2011-02-01 13:40:30 +0600

    ???????? Helper_Action_Page::obsolete

--- Class/Helper/Action/Page.php
+++ Class/Helper/Action/Page.php
@@ -5,8 +5,21 @@ class Helper_Action_Page
     
     public static function notFound ()
     {
-		$dispatcher = IcEngine::$application->frontController->getDispatcher ();
-		$dispatcher
+		IcEngine::$application
+			->frontController
+			->getDispatcher ()
+			->flushStack (true)
+			->pushArray (array (
+				'controller'	=> 'Authorization',
+				'action'		=> 'accessDenied'
+			));
+    }
+    
+    public static function obsolete ()
+    {
+		IcEngine::$application
+			->frontController
+			->getDispatcher ()
 			->flushStack (true)
 			->pushArray (array (
 				'controller'	=> 'Authorization',
