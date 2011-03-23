b4707a7ce09bbec9fdb85b446b45daf11ba5db01
???? <????@localhost>
2011-03-23 09:57:35 +0600

    ????????? Data_Validator_Email (?????? ??? ?????????? ?????? ??????).

--- Class/Data/Validator/Email.php
+++ Class/Data/Validator/Email.php
@@ -15,7 +15,9 @@ class Data_Validator_Email extends Data_Validator_Abstract
 	 */
 	public function validate ($data)
 	{
-		return $data == filter_var ($data, FILTER_VALIDATE_EMAIL);
+		return
+			$data && 
+			$data == filter_var ($data, FILTER_VALIDATE_EMAIL);
 	}
 	
 }
