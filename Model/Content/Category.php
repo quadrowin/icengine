b3acb1ee38f8b77adeff274480604154a2086c13
Ilya.k <Ilya.k@localhost>
2011-03-25 17:50:12 +0600

    ???????

--- Model/Content/Category.php
+++ Model/Content/Category.php
@@ -2,7 +2,18 @@
 
 class Content_Category extends Model
 {
-	
-	
-	
+	/**
+	 * @desc Получить коллекцию дочерних категорий
+	 * @return Model_Collection
+	 */
+	public function childs ()
+	{
+		return new Model_Collection (DDS::execute (
+			Query::instance ()
+			->from ('Content_Category')
+			->where ('parentId', $this->key ())	
+			)
+				->asTable ()
+		);
+	}
 }
