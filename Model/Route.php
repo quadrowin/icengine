7d8d0fff3c7fa9d27f5b53b2cb0d148522900fee
Ilya.k <Ilya.k@localhost>
2011-04-11 12:17:09 +0700

    ??????

--- Model/Route.php
+++ Model/Route.php
@@ -10,6 +10,50 @@ class Route extends Model_Child
     const MODEL_METHOD_GET_TITLE = 'getRouteTitle';
     
     /**
+	 * @desc Получить роут по урлу
+	 * @param string $url
+	 * @return Route
+     */
+    public static function byUrl ($url)
+    {
+   		$url = '/' . trim ($url, '/') . '/';
+		
+		/*
+		 * Заменяем /12345678/ на /?/.
+		 * Операция применяется дважды, т.к. если в запросе
+		 * несколько чисел идет подряд "/content/123/456/789/",
+		 * то в результате первого прохода вхождения будут заменены
+		 * через раз - "/content/?/456/?/", и только после второго
+		 * полностью - "/content/?/?/?/".
+		 * Это позволяет привести все запросы с переменными к одному,
+		 * который будет закеширован. 
+		 */ 
+		$template = preg_replace ('#/[0-9]{1,}/#i', '/?/', $url);
+		$template = preg_replace ('#/[0-9]{1,}/#i', '/?/', $template);
+		
+		$select = new Query ();
+		$select
+			->select (array ('Route' => array ('id', 'route', 'View_Render__id')))
+			->select (array ('View_Render' => array ('name' => 'viewRenderName')))
+			->from ('Route')
+			->from ('View_Render')
+			->where ('? RLIKE template', $template)
+			->where ('Route.View_Render__id = View_Render.id')
+			->order (array ('weight' => Query::DESC))
+			->limit (1);
+		
+		$row = DDS::execute ($select)->getResult ()->asRow ();
+		//var_dump(DDS::getDataSource()->getQuery('Mysql'), $row);
+		if (!$row)
+		{
+			return null;
+		}
+		
+		return new self ($row);
+		
+    }
+    
+    /**
      * Получение ссылки на роут
      * @return string
      */
