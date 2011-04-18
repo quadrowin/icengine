551050600f728fb16f1e2588df28f8352f7abc47
???? <????@localhost>
2011-04-18 09:01:28 +0700

    ??? ??????????? ?????? Console, Files, Get, Post, Router ?????????? ????? GetAll.

--- Class/Data/Provider/Console.php
+++ Class/Data/Provider/Console.php
@@ -1,14 +1,24 @@
 <?php
-
+/**
+ * 
+ * @desc Провайдер данных для получения параметров из консоли.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
 class Data_Provider_Console extends Data_Provider_Abstract
 {
     
     /**
-     * 
+     * @desc Разобранные параметры консоли.
      * @var array
      */
     protected $_args;
     
+    /**
+     * @desc Создает и возвращает провайдер.
+     * @param array $args Параметры консоли.
+     */
     public function __construct (array $args)
     {
         foreach ($args as $arg)
@@ -22,11 +32,28 @@ class Data_Provider_Console extends Data_Provider_Abstract
         }
     }
     
+    /**
+     * (non-PHPdoc)
+     * @see Data_Provider_Abstract::get()
+     */
 	public function get ($key, $plain = false)
 	{
 		return isset ($this->_args [$key]) ? $this->_args [$key] : null;
 	}
 	
+	/**
+	 * (non-PHPdoc)
+	 * @see Data_Provider_Abstract::getAll()
+	 */
+	public function getAll ()
+	{
+		return $this->_args;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Data_Provider_Abstract::set()
+	 */
 	public function set ($key, $value, $expiration = 0, $tags = array ())
 	{
 		echo "$key => $value\n";
