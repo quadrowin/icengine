e3d70953e4da587e545fd0c60e13df1c5cd395f1
Goorus <Goorus@localhost>
2011-04-19 23:57:30 +0700

    ????? ??? ?????? ???????? ???????.

---
+++ Class/Model/Collection/Option/Active.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 
+ * @desc Опция для выбора только активных моделей.
+ * $params ['active'] если false, будут выбраны неактивные.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+class Model_Collection_Option_Active extends Model_Collection_Option_Abstract
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Model_Collection_Option_Abstract::before()
+	 */
+	public function before (Model_Collection $collection, 
+		Query $query, array $params)
+	{
+		if (isset ($params ['active']) && !$params ['active'])
+		{
+			$query->where ($collection->modelName () . '.active', 0);
+		}
+		else
+		{
+			$query->where ($collection->modelName () . '.active', 1);
+		}
+	}
+	
+}
