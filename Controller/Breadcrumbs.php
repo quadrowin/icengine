416cbbbb80ba3a3417d32f4456dedef3eb323e04
???? <????@localhost>
2011-05-06 15:27:34 +0700

    ??????? ?????????? ??????? ??????

--- Controller/Breadcrumbs.php
+++ Controller/Breadcrumbs.php
@@ -1,13 +1,23 @@
 <?php
-
+/**
+ * 
+ * @desc Контроллер для вывода хлебных крошек.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
 class Controller_Breadcrumbs extends Controller_Abstract
 {
     
+	/**
+	 * (non-PHPdoc)
+	 * @see Controller_Abstract::index()
+	 */
     public function index ()
     {
         Loader::load ('Route_Collection');
         $path = new Route_Collection ();
-        $route = IcEngine::frontController ()->getRouter ()->getRoute ();
+        $route = IcEngine::route ();
         while ($route)
         {
             $path->add ($route);
