5638b64ca4d3fdfdbd6b59fa4c89686a981e283d
???? <????@localhost>
2011-05-20 17:25:42 +0700

    ?????? ???? ? ??????? ? ???

--- Class/Tracer/Redis/Session.php
+++ Class/Tracer/Redis/Session.php
@@ -21,13 +21,28 @@ class Tracer_Redis_Session extends Tracer_Abstract
 	 */
 	public function add ($info)
 	{
+		
 		$text = Helper_Date::toUnix () . ' ' . Request::ip () . ' ' . $info;
 		for ($i = 1; $i < func_num_args (); ++$i)
 		{
 			$text .= ' ' . json_encode (func_get_arg ($i));
 		}
 		
-		
+		if (
+			strpos ($info, 'del') !== false &&
+			class_exists ('User') &&
+			class_exists ('Request')
+		)
+		{
+			$text .= 
+				"\nuser_id:" . User::id () . "\n" .
+				"url:" . Request::uri () . "\n";
+			
+			ob_start ();
+				debug_print_backtrace ();
+				$text .= ob_get_contents ();
+			ob_end_clean ();
+		}
 		
 		$f = fopen ($this->file, 'a');
 		fwrite ($f, $text . PHP_EOL);
