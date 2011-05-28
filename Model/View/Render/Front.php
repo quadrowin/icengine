fe1ad8e6badbd80922495b4157745880cdb49e33
???? <????@localhost>
2011-05-28 16:51:58 +0700

    View_Render_Front

---
+++ Model/View/Render/Front.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 
+ * @desc Редер фронт контроллера
+ * @author Юрий Шведов, Илья Колесников
+ * @package IcEngine
+ *
+ */
+class View_Render_Front extends View_Render_Abstract
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::addHelper()
+	 */
+	public function addHelper ($helper, $method)
+	{
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::display()
+	 */
+	public function display ($tpl = null)
+	{
+		return $this->fetch ($tpl);
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::fetch()
+	 */
+	public function fetch ($tpl)
+	{
+		$render = $this->_vars ['render'];
+		$tasks = $this->_vars ['tasks'];
+		
+		return $render->render ($tasks);
+	}
+	
+}
