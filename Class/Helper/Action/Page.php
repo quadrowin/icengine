46e40152a0da668648aa379df72ae029a8ef8803
Ivan <Ivan@localhost>
2011-05-28 13:14:21 +0700

    ?????????????????? ?????????? stable ?????? ???????????? ?????????????????? 6c21eb20040e

---
+++ Class/Helper/Action/Page.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * 
+ * Стандартные реакции контроллера на ошибки
+ * @author Юрий
+ *
+ */
+
+class Helper_Action_Page
+{
+    
+	/**
+	 * Страница не найдена
+	 */
+    public static function notFound ()
+    {
+		IcEngine::frontController ()
+			->getDispatcher ()
+			->flushActions (true)
+			->push (array (
+				'controller'	=> 'Error',
+				'action'		=> 'notFound'
+			));
+    }
+    
+    /**
+     * Страница устарела
+     */
+    public static function obsolete ()
+    {
+		IcEngine::frontController ()
+			->getDispatcher ()
+			->flushActions (true)
+			->push (array (
+				'controller'	=> 'Error',
+				'action'		=> 'obsolete'
+			));
+    }
+    
+}
---
+++ Class/Model/Child.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * 
+ * @desc Модель для организации дерева.
+ * Имеет родителя и потомков. Для организации связи, в модели должно
+ * существовать поле "parentId", определяющее предка.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+abstract class Model_Child extends Model
+{
+	/**
+	 * @desc Получить коллекцию дочерних категорий
+	 * @return Model_Collection
+	 */
+	public function childs ()
+	{
+		return Model_Collection_Manager::byQuery (
+			$this->modelName (),
+			Query::instance ()
+				->where (
+					'`' . $this->modelName() . '`.parentId',
+					$this->key ()
+				)
+				->where ('`' . $this->modelName() . '`.parentId != 0')
+		);
+	}
+	
+	/**
+	 * @desc Возвращает предка.
+	 * @return Model_Child
+	 */
+	public function getParent ()
+	{
+		return $this->parentKey () ? 
+			Model_Manager::byKey ($this->modelName (), $this->parentKey ()) : 
+			null;
+	} 
+	
+	/**
+	 * 
+	 * @param integer|Model $parent
+	 * @return boolean
+	 */
+	public function hasParent ($parent)
+	{
+		if ($parent instanceof Model)
+		{
+			if (get_class ($this) != get_class ($parent))
+			{
+				return false;
+			}
+			$parent = $parent->key ();
+		}
+		
+		$current = $this->getParent ();
+		while ($current)
+		{
+			if ($current->key () == $parent)
+			{
+				return true;
+			}
+			$current = $current->getParent ();
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @desc Возвращает уровень в дереве моделей.
+	 * @param integer $rate Множитель. Результат будет домножен на указанную 
+	 * величину.
+	 * @return integer
+	 */
+	public function level ($rate = 1)
+	{
+		if ($this->parentKey ())
+		{
+			return ($this->getParent ()->level () + 1) * $rate;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	/**
+	 * @desc Возвращает значение поля с родительским ключом.
+	 * @return integer Первичный ключ родителя.
+	 */
+	public function parentKey ()
+	{
+		return $this->parentId;
+	}
+	
+	/**
+	 * @desc Возврщаает ключ корневого предка.
+	 * @return integer 
+	 */
+	public function parentRootKey ()
+	{
+		return 0;
+	}
+	
+}
