bf40f5d1b992b7687877235b0546218cb7f09b3e
Ilya.k <Ilya.k@localhost>
2011-05-28 15:53:36 +0700

    ?????? ???????????

--- Class/Controller/Dispatcher.php
+++ Class/Controller/Dispatcher.php
@@ -8,15 +8,55 @@
  */
 class Controller_Dispatcher
 {
+	/**
+	 * @desc Экшин по умолчанию
+	 * @param string
+	 */
+	const DEFAULT_ACTION = 'index';
 	
 	/**
-	 * @desc 
+	 * @desc Ищет соответствие имени файла и метода
 	 * @param string $controller
 	 * @param string $action
+	 * @return array<controller,action>
 	 */
-	public function dispatch ($controller, $action)
+	public static function dispatch ($controller, $action)
 	{
-		return 'Controller_' . $controller;
+		return array (
+			'controller'	=> $controller,
+			'action'		=> $action
+		);
 	}
-
+	
+	/**
+	 * @desc Запускает цикл диспетчеризации.
+	 * Находит соответствиие имени файла и метода
+	 * @param Model_Collection $actions
+	 * @return Controller_Action_Collection
+	 */
+	public static function loop ($actions)
+	{
+		foreach ($actions as $action)
+		{
+			$_controller = $action->controller;
+			
+			if (strpos ($_controller, '/') !== false)
+			{
+				list ($_controller, $_action) = explode ('/', $_controller);
+			}
+			else
+			{
+				$_action = isset ($action->action) ?
+					$action->action :
+					self::DEFAULT_ACTION;
+			}
+			
+			$action->set (self::dispatch (
+				$_controller,
+				$_action
+			));		
+		}
+		
+		return $actions;
+	}
 }
