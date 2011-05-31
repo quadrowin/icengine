46ffea4c38f7cc5e2a6e314c24476062e96b65f5
roman <roman@localhost>
2011-05-31 09:51:05 +0700

    $model_manager => Model_Manager

---
+++ Controller/Captcha.php
@@ -0,0 +1,21 @@
+<?php
+
+class Controller_Captcha extends Controller_Abstract
+{
+	
+	public function getCode ()
+	{
+		Loader::load ('Helper_Captcha');
+		
+		$code = Helper_Captcha::generateAutocode ();
+		
+		$_SESSION [Helper_Captcha::SF_AUTO_CODE] = $code;
+		
+		$this->_output->send ('data', array (
+			'code'	=> $code
+		));
+		
+		$this->_task->setTemplate (null);
+	}
+	
+}
