a0399bc45cc7718ced03bd18aea87cd4dbd4e05b
Ilya.k <Ilya.k@localhost>
2011-06-02 13:22:10 +0700

    Model_Defined

---
+++ Class/Model/Defined.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @desc Модель ячейки данных. Может быть уже заполнена
+ * @author Роман Кузнецов, Колесников Илья
+ * @package IcEngine
+ */
+class Model_Defined extends Model
+{
+    public static $_rows = array ();
+}
