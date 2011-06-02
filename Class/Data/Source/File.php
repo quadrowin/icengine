250d2474c02f72e051ed3668ceadd3e77db764ec
???? <????@localhost>
2011-06-02 13:26:49 +0700

    ???????

--- Class/Data/Source/File.php
+++ Class/Data/Source/File.php
@@ -2,9 +2,9 @@
 
 class Data_Source_File extends Data_Source_Abstract
 {
-	
 	public function __construct ()
 	{
+		Loader::load ('Data_Mapper_File');
 		$this->setDataMapper (new Data_Mapper_File);
 	}
 	
