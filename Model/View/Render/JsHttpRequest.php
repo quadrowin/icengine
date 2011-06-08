e581c5e0d5ede411855223f498d223e4124e6ec7
Ilya.k <Ilya.k@localhost>
2011-06-08 11:47:41 +0700

    ????? ??????

---
+++ Model/View/Render/JsHttpRequest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * 
+ * @desc Рендер данных для библиотеки JsHttpRequest
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+class View_Render_JsHttpRequest extends View_Render_Abstract
+{
+	
+	/**
+	 * @desc Экземпляр бекэнда.
+	 * @var JsHttpRequest
+	 */
+	protected $_request;
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::_afterConstruct()
+	 */
+	protected function _afterConstruct ()
+	{
+		Loader::load ('JsHttpRequest', 'includes');
+		$this->_request = new JsHttpRequest ('UTF-8');
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::fetch()
+	 */
+	public function fetch ($tpl)
+	{
+		$result = $this->_vars;
+		$this->_vars = array ();
+		return $result;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::display()
+	 */
+	public function display ($tpl)
+	{
+		$GLOBALS ['_RESULT'] = reset ($this->_vars);
+		die ();
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::addHelper()
+	 */
+	public function addHelper ($helper, $method)
+	{
+		
+	}
+	
+}
