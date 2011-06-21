1d89145f521bcbf638bd3e24e0f5778718fbc7bc
root <morph@bk.ru>
2011-06-21 11:51:13 +0200

    mimemail fix

---
+++ Class/Model/Collection/Option/Order/Asc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 
+ * @desc Опция для добавления правила упорядочивания в порядке возрастания.
+ * Возможно передать поле для сортировки, если поле не передано, сортировка
+ * будет идти по ключевому полю.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+class Model_Collection_Option_Order_Asc extends Model_Collection_Option_Abstract
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Model_Collection_Option_Abstract::before()
+	 */
+	public function before (Model_Collection $collection, 
+		Query $query, array $params)
+	{
+		$field = isset ($params ['field']) ?
+			$params ['field'] :
+			(
+				'`' . $collection->modelName () . '`.`' . 
+				$collection->keyField () . '`'
+			);
+		
+		$query->order (array ($field => Query::DESC));
+	}
+	
+}
