2ae3512ea630e2152890b880aaacaf177582aa09
???? <????@localhost>
2011-06-21 16:54:18 +0700

    ???????? ????? Request::isConsole ??? ???????? ??????? ?? ?????? ?? ????????? ??????.

--- Class/Request.php
+++ Class/Request.php
@@ -74,6 +74,16 @@ class Request
 			$_SERVER ['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
 		);
 	}
+	
+	/**
+	 * @desc Проверяет, что скрипт был вызван через консоль.
+	 * @return boolean true, если скрипт был вызван из командной строки, 
+	 * иначе - false.
+	 */
+	public static function isConsole ()
+	{
+		return isset ($_SERVER ['argv'], $_SERVER ['argc']);
+	}
 		
 	/**
 	 * @desc Проверяет, передены ли файлы от пользователя.
