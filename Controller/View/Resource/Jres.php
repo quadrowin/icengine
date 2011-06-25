6be5e7bbfba2ffc683d2069ac1cdd245ce7ec61c
???? <????@localhost>
2011-06-25 14:08:21 +0700

    Jres

---
+++ Controller/View/Resource/Jres.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 
+ * @desc Контроллер для загрузки js ресурсов.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+class Controller_View_Resource_Jres extends Controller_Abstract
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Controller_Abstract::index()
+	 */
+	public function index ()
+	{
+		$config = $this->config ();
+		
+		Loader::load ('View_Resource_Loader');
+		
+		$sources = $config->sources;
+		
+		foreach ($config->sources as $source)
+		{
+			View_Resource_Loader::load (
+				$source ['base_url'],
+				$source ['base_dir'],
+				$source ['patterns'],
+				View_Resource_Manager::JRES
+			);
+		}
+		
+		$jreses = View_Resource_Manager::getData (
+			View_Resource_Manager::JRES
+		);
+		
+		$packer = View_Resource_Manager::packer (View_Resource_Manager::JRES);
+		
+		$packer->pack ($jreses, $config->packed_file);
+		
+		$this->_output->send (array (
+			'url'	=> $config->packed_url,
+			'ts'	=> $packer->cacheTimestamp ()
+		));
+	}
+	
+}
