85ecdcfaa7ceaa52d4226c6f9c6c0d1680dc0cda
dp <dp@ds.local>
2011-06-27 16:13:50 +0700

    ???????

--- Class/Data/Validator/Not/Empty.php
+++ Class/Data/Validator/Not/Empty.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * 
- * @desc Валидатор отличия от null.
+ * @desc Валидатор истинности выражения.
  * @author Юрий Шведов
  * @package IcEngine
  *
  */
-class Data_Validator_Not_Null extends Data_Validator_Abstract
+class Data_Validator_Not_Empty extends Data_Validator_Abstract
 {
 	
 	/**
@@ -15,7 +15,7 @@ class Data_Validator_Not_Null extends Data_Validator_Abstract
 	 */
 	public function validate ($data)
 	{
-		return $data != null;
+		return (bool) $data;
 	}
 	
 }
