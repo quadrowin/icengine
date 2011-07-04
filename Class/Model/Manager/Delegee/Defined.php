842a74ee1bbf9eb2b8430a7dd0a2b57d32b34b53
Ilya.k <Ilya.k@localhost>
2011-07-04 10:49:57 +0700

    ???

---
+++ Class/Model/Manager/Delegee/Defined.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * @desc Класс для создания определенных моделей.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+class Model_Manager_Delegee_Defined
+{
+	
+	/**
+	 * @desc 
+	 * @param string $model Название модели
+	 * @param string $key Ключ (id)
+	 * @param Model|array $object Объект или данные
+	 * @return Model В случае успеха объект, иначе null.
+	 */
+	public static function get ($model, $key, $object)
+	{
+		return new $model (is_array ($object) ? $object : array ());
+	}
+	
+}
