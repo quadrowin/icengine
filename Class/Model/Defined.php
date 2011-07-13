c8eb77e8cfe3ddae9c9910e19fb5420903cd10e1
markov <markov@localhost>
2011-07-13 14:43:36 +0700

    unit test Config_Array

---
+++ Class/Model/Defined.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @desc Модель ячейки данных. Может быть уже заполнена
+ * @author Роман Кузнецов, Колесников Илья
+ * @package IcEngine
+ */
+class Model_Defined extends Model
+{
+	
+	/**
+	 * @desc 
+	 * @var array
+	 */
+    public static $rows = array ();
+    
+}
---
+++ Controller/Render.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @desc Контролер для рендеринга заданий
+ * @author Юрий Шведов, Илья Колесников
+ * @package IcEngine
+ */
+class Controller_Render extends Controller_Abstract
+{
+	/**
+	 * @desc Рендерим
+	 */
+	public function index ()
+	{
+		list (
+			$task,
+			$render
+		) = $this->_input->receive (
+			'task',
+			'render'
+		);
+		
+//		$render->assign (array (
+//			'task'	=> $task
+//		));
+		$render->render ($task);
+	}
+}
