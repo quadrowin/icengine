b7126e3a5e263505ac0b89ccdd9e3a7a3168feb7
dp <dp@ds.local>
2011-07-28 19:33:19 +0700

    ????

--- Class/Model/Manager.php
+++ Class/Model/Manager.php
@@ -315,7 +315,7 @@ class Model_Manager extends Manager_Abstract
 			$result
 		);
 		
-		return $result->key () ? $result : $generic;
+		return $result->key () ? $result : new $model (array ());
 	}
 	
 	/**
