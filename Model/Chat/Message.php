e825d21332b07836a7f0a0f68eda26c73c3a11fa
Ilya.k <Ilya.k@localhost>
2011-07-29 12:52:00 +0600

    ????

--- Model/Chat/Message.php
+++ Model/Chat/Message.php
@@ -2,5 +2,18 @@
 
 class Chat_Message extends Model
 {
-	
+	public function getNew ()
+	{
+		return Model_Manager::byQuery (
+			'Chat_Message',
+			Query::instance ()
+				->innerJoin (
+					'Chat_Session_Join',
+					'Chat_Session_Join.id=Chat_Message.Chat_Session_Join__id'
+				)
+				->where ('phpSessionId', User_Session::getCurrent ()
+					->phpSessionId)
+				->where ('readed', 0)
+		);
+	}
 }
