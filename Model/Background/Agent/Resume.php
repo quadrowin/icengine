19ed32c4aba0eff095ca026ad620822fe4f8e4f4
???? ?????? <???? ??????@pc13>
2011-08-04 20:39:12 +0700

    ?????? ??? ???????? ??????? ??????, ?????? ???????????? ??????????? ?????.

---
+++ Model/Background/Agent/Resume.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * @desc Фабрика для моделей возобновления процессов.
+ * @author Юрий Шведов
+ * @package IcEngine
+ *
+ */
+class Background_Agent_Resume extends Model_Factory
+{
+	
+}
---
+++ Model/Content/Category/Collection.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @desc Коллекция категорий (разделов) контента.
+ * @package IcEngine
+ * 
+ */
+class Content_Category_Collection extends Model_Collection
+{
+	
+}
