9795dcdf356952ce5788e7e52d228efb105eb96b
???? ?????? <???? ??????@progr5>
2011-08-17 14:47:18 +0700

    Merge

--- Model/Temp/Content.php
+++ Model/Temp/Content.php
@@ -23,10 +23,9 @@ class Temp_Content extends Model
 	 */
 	public static function byUtcode ($utcode)
 	{
-		return Model_Manager::byQuery (
+		return Model_Manager::byKey (
 			__CLASS__,
-			Query::instance ()
-				->where ('utcode', (string) $utcode)
+			(string) $utcode
 		);
 	}
 	
@@ -97,6 +96,11 @@ class Temp_Content extends Model
 		$u = uniqid ('', true);
 		// Вырезаем точку
 		return 'uc' . md5 (time ()) . substr ($u, 9, 5) . substr ($u, 15);
+	}
+	
+	public static function idForNew (Temp_Content $tc)
+	{
+		return $tc->utcode;
 	}
 	
 	/**
