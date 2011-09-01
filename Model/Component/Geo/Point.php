8d4414392330700e664ca62667e49294da61238d
???? ?????? <???? ??????@pc13>
2011-09-01 09:30:21 +0700

    Component_Geo_Point->toJs

--- Model/Component/Geo/Point.php
+++ Model/Component/Geo/Point.php
@@ -4,8 +4,20 @@ Loader::load ('Model_Component');
 
 class Component_Geo_Point extends Model_Component
 {
+	
 	public function style ()
 	{
 		return $this->Geo_Point_Style->name;
 	}
+	
+	public function toJs ()
+	{
+		return array (
+			'id'		=> $this->key (),
+			'longitude'	=> $this->longitude,
+			'latitude'	=> $this->latitude,
+			'title'		=> $this->name
+		);
+	}
+	
 }
