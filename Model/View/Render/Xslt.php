036a6fe76b6d04a5d5744f55b6edbc4c9096be4f
???? ?????? <???? ??????@pc13>
2011-09-15 15:30:08 +0700

    ???????? ?????? Xslt

--- Model/View/Render/Xslt.php
+++ Model/View/Render/Xslt.php
@@ -14,28 +14,7 @@ class View_Render_Xslt extends View_Render_Abstract
 	 * @var array
 	 */
 	protected static $_config = array (
-		/**
-		 * @desc Директория для скопилированных шаблонов Smarty
-		 * @var string
-		 */
-		'compile_path'		=> 'cache/templates',
-		/**
-		 * @desc Пути до шаблонов
-		 * @var array
-		 */
-		'templates_path'	=> array (),
-		/**
-		 * @desc Пути до плагинов
-		 * @var array
-		 */
-		'plugins_path'		=> array (),
-		/**
-		 * @desc Фильры
-		 * @var array
-		 */
-		'filters'			=> array (
-			'Dblbracer'
-		)
+		
 	);
 	
 	/**
@@ -44,115 +23,12 @@ class View_Render_Xslt extends View_Render_Abstract
 	 */
 	protected $_processor;
 	
+	/**
+	 * @desc Инициализация процессора
+	 */
 	protected function _afterConstruct ()
 	{
-		$config = $this->config ();
-		
 		$this->_processor = new XSLTProcessor ();
-		
-		$this->_templatesPathes = array_reverse (
-			$config ['templates_path']->__toArray ()
-		);
-		
-		// Фильтры
-		foreach ($config ['filters'] as $filter)
-		{
-			$filter = 'Helper_Smarty_Filter_' . $filter;
-			Loader::load ($filter);
-			$filter::register ($this->_smarty);
-		}
-	}
-	
-	/**
-	 * @desc Получает идентификатор компилятор для шаблона.
-	 * Необходимо, т.к. шаблон зависит от путей шаблонизатора.
-	 * @param string $tpl
-	 * @return string
-	 */
-	protected function _compileId ($tpl)
-	{
-		return crc32 (json_encode ($this->_smarty->template_dir));
-	}
-	
-	/**
-	 * @desc Добавление пути до директории с плагинами Smarty
-	 * @param string|array $path Директории с плагинами
-	 */
-	public function addPluginsPath ($path)
-	{
-		$this->_smarty->plugins_dir = array_merge (
-			(array) $this->_smarty->plugins_dir,
-			(array) $path
-		);
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see View_Render_Abstract::addTemplatesPath()
-	 */
-	public function addTemplatesPath ($path)
-	{
-		$this->_smarty->template_dir = array_merge (
-			array_reverse ((array) $path),
-			(array) $this->_smarty->template_dir
-		);
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see View_Render_Abstract::addHelper()
-	 */
-	public function addHelper ($helper, $method)
-	{
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see View_Render_Abstract::assign()
-	 */
-	public function assign ($key, $value = null)
-	{
-		if (is_array ($key))
-		{
-			$this->_smarty->assign ($key);
-		}
-		else
-		{
-			$this->_smarty->assign ($key, $value);
-		}
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see View_Render_Abstract::display()
-	 */
-	public function display ($tpl)
-	{
-		ob_start ();
-		$this->_processor->importStylesheet ($tpl);
-		$this->_processor->transformToURI ($this->xml (), 'php://output');
-		echo ob_get_flush ();
-	}
-	
-	/**
-	 * (non-PHPdoc)
-	 * @see View_Render_Abstract::fetch()
-	 */
-	public function fetch ($tpl)
-	{
-		ob_start ();
-		$this->_processor->importStylesheet ($tpl);
-		$this->_processor->transformToURI ($this->xml (), 'php://output');
-		return ob_get_flush ();
-	}
-	
-	/**
-	 * @desc Возвращает используемый экземпляр шаблонизатора.
-	 * @return Smarty
-	 */
-	public function processor ()
-	{
-		return $this->_processor;
 	}
 	
 	/**
@@ -182,6 +58,40 @@ class View_Render_Xslt extends View_Render_Abstract
 	}
 	
 	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::display()
+	 */
+	public function display ($tpl)
+	{
+		ob_start ();
+		$this->_processor->importStylesheet ($tpl);
+		$this->_processor->transformToURI ($this->xml (), 'php://output');
+		echo ob_get_flush ();
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see View_Render_Abstract::fetch()
+	 */
+	public function fetch ($tpl)
+	{
+		ob_start ();
+		$this->_processor->importStylesheet ($tpl);
+		$this->_processor->transformToURI ($this->xml (), 'php://output');
+		return ob_get_flush ();
+	}
+	
+	/**
+	 * @desc Возвращает используемый экземпляр шаблонизатора.
+	 * @return XSLTProcessor
+	 */
+	public function processor ()
+	{
+		return $this->_processor;
+	}
+	
+	/**
+	 * @desc Формирует XML документ, содержащий данные для вывода
 	 * @return DOMDocument
 	 */
 	public function xml ()
