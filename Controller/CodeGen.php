657d90643d2fb55bfe4534d0647c44fff05aa0df
Ilya.k <Ilya.k@localhost>
2011-10-26 10:20:07 +0600

    ??? ?????????, ???

--- Controller/CodeGen.php
+++ Controller/CodeGen.php
@@ -25,4 +25,21 @@ class Controller_CodeGen extends Controller_Abstract
 		
 		$this->_task->setTemplate (null);
 	}
+	
+	public function createProject ($config_path)
+	{
+		Loader::load ('Helper_Code_Generator');
+		Loader::load ('Config_Php');
+		
+		$config = new Config_Php ($config_path);
+		
+		if ($config)
+		{
+			$config = $config->__toArray ();
+		}
+		
+		Helper_Code_Generator::createProject ($config);
+		
+		$this->_task->setTemplate (null);
+	}
 }
