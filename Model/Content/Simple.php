238b551b5239435ebfa83cdea17494f5c8209e43
morph <morph@morph.ic>
2011-11-29 14:38:22 +0600

    ! fix ????? base ??? Content_Simple ?????? ????????????? base

--- Model/Content/Simple.php
+++ Model/Content/Simple.php
@@ -1,32 +1,19 @@
 <?php
 /**
- * 
+ *
  * @desc Базовый класс контента
  * @author Юрий Шведов
  * @package IcEngine
- * 
+ *
  */
 
 Loader::load ('Content_Abstract');
 
 class Content_Simple extends Content_Abstract
 {
-    	public function base ()
-	{
-		return Model_Manager::byKey (
-			'Content',
-			$this->key ()
-		);
-	}
-	
-    /**
-     * @see Content_Extending::extending
-     */
-    //public function extending ()
-    //{
-    //    
-    //}
-
+	/**
+	 * @see Content_Abstract::modelName
+	 */
 	public function modelName ()
 	{
 		return 'Content';
