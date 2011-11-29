0c65983237caec808397217671da1643486e57e7
Shvedov_U <Shvedov_U@utkonos.grs>
2011-11-29 11:31:26 +0400

    + ????? Application

---
+++ App/Class/Application.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * @desc Приложение.
+ * Для корректной работы потомки должны находиться
+ * в соответвующих приложению пространстве имен в директории "App/Class".
+ * @author Shvedov_U
+ * @package Ice
+ *
+ */
+class Application {
+
+	/**
+	 * @desc Пути до приложений
+	 * @var array
+	 */
+	protected static $_dirs = array ();
+
+	/**
+	 * @desc Возвращает путь до корня приложения.
+	 * Путь до приложения отличается от пути до корня сайта.
+	 * @return string Путь до корня приложения
+	 */
+	public static function getDir ()
+	{
+		$class = get_called_class ();
+		if (!isset (self::$_dirs [$class]))
+		{
+			$r = new \ReflectionClass ($class);
+			self::$_dirs [$class] = realpath (
+				dirname ($r->getFileName ()) . '/../..'
+			);
+		}
+		return self::$_dirs [$class];
+	}
+
+}
