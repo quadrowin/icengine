c507323b9f6c7c386102373d64427a3928c15afd
red <red@localhost>
2011-12-07 17:09:25 +0600

    ???? ? Controller_Abstract.php, ????? _sendError: ??-?? ??????????? ????????? ?????????? ???????? ????????? $tpl ????? ??????? ?????????????? ????? ?????????. ????????? ??????????? ????????.

---
+++ Class/Data/Validator/Captcha/Auto/Code.php
@@ -0,0 +1,25 @@
+<?php
+
+class Data_Validator_Captcha_Auto_Code extends Data_Validator_Abstract
+{
+	
+	const FAIL = 'fail';
+	
+	public function validate ($data)
+	{
+		Loader::load ('Helper_Captcha');
+		
+		if (
+			!isset ($data, $_SESSION [Helper_Captcha::SF_AUTO_CODE]) ||
+			$_SESSION [Helper_Captcha::SF_AUTO_CODE] != $data
+		)
+		{
+			return __CLASS__ . '/' . self::FAIL;
+		}
+		
+		unset ($_SESSION [Helper_Captcha::SF_AUTO_CODE]);
+		
+		return true;
+	}
+	
+}
