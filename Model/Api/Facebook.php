2526ffffa326bf42abc9c35fb6b62d29be006e4d
dp <dp@ds.local>
2011-12-20 17:29:45 +0600

    ???? ??? ??????? ?????? (??? '__id')

---
+++ Model/Api/Facebook.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * @desc Модель поддержки API Facebook
+ * @author Юрий Шведов
+ * @package IcEngine
+ * 
+ */
+class Api_Facebook extends Api_Abstract
+{
+	//put your code here
+}
---
+++ Model/Component/Geo/Polyline/Collection.php
@@ -0,0 +1,8 @@
+<?php
+
+Loader::load ('Component_Collection');
+
+class Component_Geo_Polyline_Collection extends Component_Collection
+{
+	
+}
