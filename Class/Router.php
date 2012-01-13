b0f83553c19a1a702649b18131868cdd04f5989f
morph <morph@morph.ic>
2012-01-13 18:42:59 +0700

    ??????? ????? ??? ??????? ?????

--- Class/Router.php
+++ Class/Router.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * @desc Механизм определения роута по адресу
  * @author Юрий Шведов
  * @package IcEngine
@@ -8,28 +8,36 @@
  */
 class Router
 {
-	
+
 	/**
 	 * @var Route
 	 */
 	private static $_route;
 
 	/**
+	 * @desc Обнулить текущий роут
+	 */
+	public static function clearRoute ()
+	{
+		self::$_route = null;
+	}
+
+	/**
 	 * @desc Разбирает запрос и извлекат параметры согласно
 	 * @return Route
 	 */
-	public static function getRoute () 
+	public static function getRoute ()
 	{
-		if (is_null (self::$_route)) 
+		if (is_null (self::$_route))
 		{
 			$url = Request::uri ();
-	
+
 			$gets = Request::stringGet ();
-	
+
 			if ($gets)
 			{
 				$gets = (array) explode ('&', $gets);
-				
+
 				foreach ($gets as $get)
 				{
 					if (strpos ($get, '=') === false)
@@ -41,25 +49,25 @@ class Router
 						$tmp = explode ('=', $get);
 						$_REQUEST [$tmp [0]] = $_GET [$tmp [0]] = $tmp [1];
 					}
-				} 
+				}
 			}
-			
+
 			$url = $url ? $url : '/';
-			
+
 			$route = (array) explode ('/', trim ($url, '/'));
-			
+
 			Loader::load ('Route');
 			self::$_route = Route::byUrl ($url);
 
 			if (!self::$_route)
 			{
 				return;
-			} 
-			
+			}
+
 			$parts = (array) explode ('/', trim (self::$_route->route, '/'));
-			
+
 			$len = min (sizeof ($route), sizeof ($parts));
-			
+
 			for ($i = 0; $i < $len; $i++)
 			{
 				$st = strpos ($parts [$i], ':');
@@ -67,12 +75,12 @@ class Router
 				if ($st !== false)
 				{
 					Request::param (
-						substr ($parts [$i], $st + 1), 
+						substr ($parts [$i], $st + 1),
 						isset ($route [$i]) ? substr ($route [$i], $st) : 0
 					);
 				}
 			}
-			
+
 			if (isset (self::$_route->params))
 			{
 				foreach (self::$_route->params as $param => $value)
@@ -83,5 +91,5 @@ class Router
 		}
 		return self::$_route;
 	}
-	
+
 }
