a06ed4ad9f2960b935e174c5693211886c7b79a7
morph <morph@morph.ic>
2012-02-20 17:24:20 +0700

    fix

--- js/Helper/Form.js
+++ js/Helper/Form.js
@@ -2,19 +2,19 @@
  * Помощник работы с формой.
  */
 var Helper_Form = {
-	
+
 	/**
 	 * Последняя форма, на которой запущен лоадинг
 	 */
 	$lastLoadings: null,
-	
+
 	_toggleTextForPassword: function ($password)
 	{
 		if ($password.data ('tps_input'))
 		{
 			return $password.data ('tps_input');
 		}
-		
+
 		var elPosition = $password.position ();
 		var $input = $('<input/>', {
 	    	'class': "hidden",
@@ -25,21 +25,21 @@ var Helper_Form = {
 	    	},
 	    	type: 'text'
 	    });
-		
+
 		$input.data ('tps_password', $password);
 		$password.data ('tps_input', $input);
 		$input.appendTo ($password.parent ());
-		
+
 		return $input;
 	},
-	
+
 	ajaxPost: function ($form, url, callback)
 	{
 		var data = Helper_Form.asArray ($form);
-		
+
 		JsHttpRequest.query (url, data, callback, true);
 	},
-	
+
 	/**
 	 * @desc Все поля формы как поля объекта
 	 * @param jQuery $form Форма.
@@ -48,17 +48,17 @@ var Helper_Form = {
 	 */
 	asArray: function ($form, filter)
 	{
-		$fields = 
+		$fields =
 			$form.find ('input,select,textarea')
 			.filter (':not(.nosubmit)');
-		
+
 		if (filter)
 		{
 			$fields.filter (filter);
 		}
-		
-		var data = {};	
-		
+
+		var data = {};
+
 		$fields.each (function () {
 
             /**
@@ -139,7 +139,7 @@ var Helper_Form = {
 		});
 		return data;
 	},
-	
+
 	defaultCallback: function ($form, result)
 	{
 		if (!result)
@@ -147,12 +147,12 @@ var Helper_Form = {
 			Helper_Form.stopLoading ($form);
 			return;
 		}
-		
+
 		if (result.data && result.data.alert)
 		{
 			alert (result.data.alert);
 		}
-		
+
 		if (result.html)
 		{
 			if (result.data && result.data.removeForm)
@@ -165,7 +165,7 @@ var Helper_Form = {
 				{
 					$('[name=' + result.data.field + ']').addClass ("err");
 				}
-				
+
 				// последнее вхождение ".result-msg", позволяет
 				// подменять нижнюю часть формы, которая будет содержать
 				// новый .result-msg
@@ -178,9 +178,9 @@ var Helper_Form = {
 				while ($subdiv.length)
 				{
 					$div = $subdiv;
-					$subdiv = $div.find ('.result-msg'); 
+					$subdiv = $div.find ('.result-msg');
 				}
-				
+
 				$div.html (result.html);
 				$div.show ();
 				Helper_Form.stopLoading ($form);
@@ -193,7 +193,7 @@ var Helper_Form = {
 				$form.parent ().remove ();
 			}
 		}
-		
+
 		if (result.data && result.data.redirect)
 		{
 			setTimeout (
@@ -203,13 +203,13 @@ var Helper_Form = {
 				1000
 			);
 		};
-		
+
 		if (typeof (Controller_Captcha) != "undefined")
 		{
 			Controller_Captcha.regenerateACodes ($form);
 		}
 	},
-	
+
 	/**
 	 * @desc Отправка формы по умолчанию
 	 * @param jQuery $form Форма или элемент формы.
@@ -218,7 +218,6 @@ var Helper_Form = {
 	defaultSubmit: function ($form, action)
 	{
 		$form = $form.closest ('form');
-		
 		function callback (result)
 		{
 			Helper_Form.defaultCallback ($form, result);
@@ -232,30 +231,30 @@ var Helper_Form = {
 //				window.location.href = result.redirect;
 //			}
 		}
-		
+
 		if (!action)
 		{
 			action = $form.attr ('onsubmit');
-			
+
 			var p1 = action.indexOf ('(');
 			var p2 = action.indexOf (' ');
-			
+
 			action = action.substring (
 				"Controller_".length,
 				(0 < p2 && p2 < p1) ? p2 : p1
 			);
 			action = action.replace (".", "/");
 		}
-		
+
 		Controller.call (
 			action,
 			Helper_Form.asArray ($form),
 			callback, true
 		);
 	},
-	
+
 	/**
-	 * 
+	 *
 	 * @param jQuery $owner
 	 * @returns jQuery input[type=file]
 	 */
@@ -263,13 +262,13 @@ var Helper_Form = {
 	{
 		var $input = $('input[type="file"]', $owner);
 		var is_new = false;
-		
+
 		if ($input.length == 0)
 		{
 			$input = $('<input type="file" />');
-			is_new = true;			
+			is_new = true;
 		};
-		
+
 		$input.css ({
 			position: "absolute",
 			opacity: 0.0,
@@ -279,60 +278,60 @@ var Helper_Form = {
 			padding: 0,
 			width: "220px"
 		});
-		
+
 		if (is_new)
 		{
 			$owner.append ($input);
 		};
-		
+
 		$owner.data ('input_file_upload', $input);
 		$owner.css ('overflow', 'hidden');
-		
+
 		var pos = $owner.css ('position');
 		if (pos != "relative" && pos != "absolute")
 		{
 			$owner.css ('position', 'relative');
 		};
-		
+
 		$owner.bind ('mousemove', function (event) {
 			var $input = $(event.currentTarget).data ('input_file_upload');
-			
+
 			var y = event.pageY - $owner.offset ().top - 5 + 'px';
 			var x = event.pageX - $owner.offset ().left - 170 + 'px';
-			
+
 			$input.css ({left: x, top: y});
 		});
-		
+
 		return $input;
 	},
-	
+
 	initTogglePasswordStars: function ($from)
 	{
 		if (!$from)
 		{
 			$form = $('body');
 		}
-		
-		$checkboxes = $('input[type="checkbox"].tps_checkbox', $from); 
-		
+
+		$checkboxes = $('input[type="checkbox"].tps_checkbox', $from);
+
 		$checkboxes.each (function () {
 			$(this).bind (
 				'click',
 				function ()
 				{
 					var $this = $(this);
-					
+
 					if ($this.data ('tps_text'))
 					{
 						Helper_Form.togglePasswordStars ($this, $this.data ('tps_text'));
 						return;
 					}
-					
+
 					var $parent = $this.parent ();
 					while ($parent.length)
 					{
 						var $tps_password = $parent.find ('.tps_password');
-						
+
 						if ($tps_password.length)
 						{
 							var $tps_input = Helper_Form._toggleTextForPassword ($tps_password);
@@ -347,7 +346,7 @@ var Helper_Form = {
 			);
 		});
 	},
-	
+
 	/**
 	 * Генерация уникальных ID для элементов
 	 * Необходимо для чекбоксов в IE: если 2 чекбокса имеют одинаковый id,
@@ -363,7 +362,7 @@ var Helper_Form = {
 			$(this).attr ('id', id);
 		});
 	},
-	
+
 	/**
 	 * Заменить элементы управления на див загрузки
 	 * @param jQuery $controls
@@ -375,7 +374,7 @@ var Helper_Form = {
 			$(this).append ('<div class="loading"></div>');
 		});
 	},
-	
+
 	/**
 	 * Окончание процесса загрузки
 	 * @param jQuery $form
@@ -386,7 +385,7 @@ var Helper_Form = {
 		{
 			$form = Helper_Form.$lastLoadings;
 		}
-		
+
 		if (
 			typeof ($form) != 'object' || $form == null ||
 			typeof ($form.find) != 'function'
@@ -394,19 +393,19 @@ var Helper_Form = {
 		{
 			return ;
 		}
-		
+
 		$form.find ('div.loading').remove ();
-		
+
 		Helper_Form.$lastLoadings = null;
 	},
-	
+
 	/**
 	 * Показать/скрыть пароль за звездочками
-	 * 
-	 * Если не передан второй атрибут, то его id должен быть равен 
+	 *
+	 * Если не передан второй атрибут, то его id должен быть равен
 	 * "tps_%id_чекбокса%".
-	 * 
-	 * Если первый атрибут null, второй атрибут обязателен. Состояние 
+	 *
+	 * Если первый атрибут null, второй атрибут обязателен. Состояние
 	 * поля будет изменено на противоположное.
 	 */
 	togglePasswordStars: function ($checkbox, $input)
@@ -414,7 +413,7 @@ var Helper_Form = {
 		var checked;
 		var $tps_input;
 		var $tps_password;
-		
+
 		if ($checkbox)
 		{
 			$tps_input = $checkbox.data ('tps_input');
@@ -435,7 +434,7 @@ var Helper_Form = {
 				$tps_password = $input.data ('tps_password');
 			}
 		};
-		
+
 		if (checked)
 		{
 			$tps_password.val ($tps_input.val ()).show ();
@@ -447,5 +446,5 @@ var Helper_Form = {
 			$tps_password.hide ();
 		}
 	}
-		
+
 };
