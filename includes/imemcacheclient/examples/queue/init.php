fd8611757e3e3d2d02cb9496b8d1891bc0ff3a5b
dp <dp@home-??>
2012-02-22 08:45:58 +0700

    ?????? ?????????

--- includes/imemcacheclient/examples/queue/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-setlocale(LC_ALL,'ru_RU.UTF-8');
-include dirname(__FILE__).'/../../IMemcacheClient.class.php';
-$memcache = new IMemcacheClient;
-$memcache->addServer('127.0.0.1','11211');
-$queue = $memcache->queue('test',TRUE,3600);
-$queue->strategy = 1;
