dce7335257bd216f83e74798427d21489c670980
morph <morph@morph.ic>
2012-02-28 19:02:10 +0700

    fix

--- Class/Tracer.php
+++ Class/Tracer.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * 
+ *
  * @desc Трейсер
  * @author Юрий Шведов, Илья Колесников
  * @package IcEngine
- * 
+ *
  */
 class Tracer
 {
@@ -13,25 +13,25 @@ class Tracer
 	 * @var boolean
 	 */
 	public static $enabled = true;
-	
+
 	/**
 	 * @desc Сливать ли в файл для каждой
-	 * @var type 
+	 * @var type
 	 */
 	public static $flushPerSession = true;
-	
+
 	/**
 	 * @desc
 	 * @var array
 	 */
 	public static $sessions = array ();
-	
+
 	/**
-	 * @desc 
+	 * @desc
 	 * @var integer
 	 */
 	public static $currentSession = 0;
-	
+
 	/**
 	 * @desc Начало блока
 	 */
@@ -41,16 +41,16 @@ class Tracer
 		{
 			return;
 		}
-		
+
 		self::$currentSession++;
-		
+
 		self::$sessions [self::$currentSession] = array (
 			'args'	=> func_get_args (),
 			'begin'	=> microtime (true),
 			'logs'	=> array ()
 		);
 	}
-	
+
 	/**
 	 * @desc Окончание блока
 	 */
@@ -60,18 +60,18 @@ class Tracer
 		{
 			return;
 		}
-		
+
 		$args = func_get_args ();
-		
+
 		if ($args)
 		{
 			self::log ($args);
 		}
-		
+
 		self::$sessions [self::$currentSession]['end'] = microtime (true);
-		
+
 		self::$currentSession--;
-		
+
 		if (self::$flushPerSession)
 		{
 			self::flush (
@@ -80,7 +80,7 @@ class Tracer
 			);
 		}
 	}
-	
+
 	/**
 	 * @desc запись в лог метки
 	 */
@@ -90,31 +90,31 @@ class Tracer
 		{
 			return;
 		}
-		
+
 		if (!isset (self::$sessions [self::$currentSession]['logs']))
 		{
 			self::$sessions [self::$currentSession]['logs'] = array ();
 		}
-		
+
 		$time = microtime (true);
-		
+
 		$logs = self::$sessions [self::$currentSession]['logs'];
-		
+
 		$current_index = sizeof ($logs);
-		
+
 		$delta = $time - (
 			isset ($logs [$current_index - 1])
 				? $logs [$current_index - 1]['time']
 				: self::$sessions [self::$currentSession]['begin']
 			);
-		
+
 		self::$sessions [self::$currentSession]['logs'][] = array (
 			'args'	=> func_get_args (),
 			'time'	=> $time,
 			'delta'	=> $delta
 		);
 	}
-	
+
 	/**
 	 * @desc Вывод полного лога
 	 */
@@ -124,13 +124,16 @@ class Tracer
 		{
 			return;
 		}
-		
+
+		$file_name = IcEngine::root () . 'log/tracer';
+		file_put_contents ($file_name, '');
+
 		foreach (self::$sessions as $session)
 		{
 			self::flush ($session);
 		}
 	}
-	
+
 	/**
 	 * @desc Вывод
 	 * @param array $session
@@ -142,32 +145,32 @@ class Tracer
 		{
 			return;
 		}
-		
+
 		$file_name = IcEngine::root () . 'log/tracer';
-		
-		$offset = $offset 
+
+		$offset = $offset
 			? str_repeat ("\t", $offset)
 			: '';
-		
-		$output  = 
+
+		$output  =
 			$offset . 'Begin: ' . date ('Y-m-d H:i:s', $session ['begin']) .
 			PHP_EOL .
-			$offset . 'End: ' . date ('Y-m-d H:i:s', $session ['end']) . 
+			$offset . 'End: ' . date ('Y-m-d H:i:s', $session ['end']) .
 			PHP_EOL .
-			$offset . 'Delta: ' . round ($session ['end'] - $session ['begin'], 4) . 
+			$offset . 'Delta: ' . round ($session ['end'] - $session ['begin'], 4) .
 			PHP_EOL .
-			$offset . 'Args: ' . json_encode ($session ['args']) . 
+			$offset . 'Args: ' . json_encode ($session ['args']) .
 			PHP_EOL .
 			$offset . 'Logs: ' . PHP_EOL;
 
 		foreach ($session ['logs'] as $i => $log)
 		{
-			$output .= $offset . '#' . $i . ' '. 
-				round ($log ['time'], 4) . ' ' . 
-				round ($log ['delta'], 4) . ' ' . 
+			$output .= $offset . '#' . $i . ' '.
+				round ($log ['time'], 4) . ' ' .
+				round ($log ['delta'], 4) . ' ' .
 				json_encode ($log ['args']) . PHP_EOL;
 		}
-		
+
 		file_put_contents ($file_name, $output, FILE_APPEND);
 	}
 }
