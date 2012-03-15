443226103c06d03ef34ab343c7ba16d67ab85912
red <red@localhost>
2012-03-15 13:48:09 +0700

    tests fo mobile site

---
+++ Class/Controller/Front/Strategy/Simple.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ice;
+
+Loader::load ('Controller_Front_Strategy_Abstract');
+
+/**
+ * @desc Стратегия фронтконтроллера по умолчанию
+ * @author Илья Колесников
+ */
+class Controller_Front_Strategy_Simple extends \Controller_Front_Strategy_Abstrac
+{
+
+}
---
+++ Class/SiteMap.php
@@ -0,0 +1,22 @@
+<?php
+
+class SiteMap 
+{
+	
+	/**
+	 * @return Route_Collection
+	 */
+	public static function asList ()
+	{
+		Loader::load ('Route_Collection');
+		$routes = new Route_Collection ();
+		$routes->
+			where ('visible=1')->
+			where ('viewRenderId=1');
+			
+		$routes->items ();
+			
+		return $routes->sortByParent ();
+	}
+	
+}
