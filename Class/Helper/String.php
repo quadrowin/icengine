c343f1e2309fb6356689a60784a62a90fe4c7462
neon <neon@localhost>
2012-04-11 09:22:46 +0700

    mobile engine

--- Class/Helper/String.php
+++ Class/Helper/String.php
@@ -539,6 +539,28 @@ class Helper_String
 	    }
 	}
 	
+    /**
+     * @desc ucfirst с учётом кодировки 
+     * @param string $str
+     * @param string $encoding
+     * @param bool  $lower_str_end
+     * @return type 
+     */
+    public function mb_ucfirst ($str, $encoding = "UTF-8", $lower_str_end = false){
+
+        $first_letter = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding);
+        $str_end = "";
+        if ($lower_str_end) {
+            $str_end = mb_strtolower(mb_substr($str, 1, mb_strlen($str, $encoding), $encoding), $encoding);
+        }
+        else 
+        {
+            $str_end = mb_substr($str, 1, mb_strlen($str, $encoding), $encoding);
+        }
+        $str = $first_letter . $str_end;
+
+        return $str;
+    }
 	
 }
 
