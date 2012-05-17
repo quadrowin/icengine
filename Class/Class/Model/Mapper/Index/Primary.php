ac134d86d54822145c4f8a0e5183c47f92a71d0e
neon <neon@localhost>
2012-05-17 10:06:53 +0700

    21403

---
+++ Class/Class/Model/Mapper/Index/Primary.php
@@ -0,0 +1,8 @@
+<?php
+
+Loader::load ('Model_Mapper_Index_Abstract');
+
+class Model_Mapper_Index_Primary extends Model_Mapper_Index_Abstract
+{
+	
+}
---
+++ Class/Model/Mapper/Scheme/Accessor/Reference.php
@@ -0,0 +1,21 @@
+<?php
+
+Loader::load ('Model_Mapper_Scheme_Accessor_Abstract');
+
+/**
+ * @desc Аксессор ссылок для схемы моделей
+ * @author Илья Колесников
+ */
+class Model_Mapper_Scheme_Accessor_Reference extends Model_Mapper_Scheme_Accessor_Abstract
+{
+	/**
+	 * @see Model_Mapper_Scheme_Accessor_Abstract::get
+	 */
+	public function get ($scheme, $entity)
+	{
+		return $entity->getValue ()->data (
+			$scheme->getModel ()->modelName (),
+			$scheme->getModel ()->key ()
+		);
+	}
+}
