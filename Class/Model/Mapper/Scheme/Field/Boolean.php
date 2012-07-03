055a0e59fa747a296f9e4e4d4fa5ecef60f3daac
morph <morph@morph.ic>
2012-07-03 16:12:29 +0700

    **

---
+++ Class/Model/Mapper/Scheme/Field/Boolean.php
@@ -0,0 +1,18 @@
+<?php
+
+Loader::load ('Model_Mapper_Scheme_Field_Abstract');
+
+/**
+ * @desc Тип поля boolean схемы связей модели
+ * @author Илья Колесников
+ */
+class Model_Mapper_Scheme_Field_Boolean extends Model_Mapper_Scheme_Field_Abstract
+{
+	/**
+	 * @see Model_Mapper_Scheme_Field_Abstract::validate
+	 */
+	public function validate ($value)
+	{
+		return is_bool ($value);
+	}
+}
---
+++ Model/Component/Geo/Point/Collection.php
@@ -0,0 +1,8 @@
+<?php
+
+Loader::load ('Component_Collection');
+
+class Component_Geo_Point_Collection extends Component_Collection
+{
+	
+}
