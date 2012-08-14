32e4a2e64c40584dfc61995461883974c56ecd26
neon <neon@localhost>
2012-08-14 14:42:53 +0700

    f

--- Model/Component/Comment/Option/Link.php
+++ Model/Component/Comment/Option/Link.php
@@ -1,19 +1,27 @@
 <?php
 
+/**
+ * Опшен для получения коллекции компонентов
+ *
+ * @author neon
+ */
 class Component_Comment_Option_Link extends Model_Option
 {
 
 	/**
-	 *
-	 * @param Model_Collection $items
-	 * @param Query $query
-	 * @param array $params
+	 * @inheritdoc
 	 */
 	public function before ()
 	{
-		$this->query
-			->where ('table', $this->params ['table'])
-			->where ('rowId', $this->params ['rowId']);
+		if (isset ($this->params['table']))
+		{
+			$this->query
+				->where ('table', $this->params['table']);
+		}
+		if (isset ($this->params['rowId']))
+		{
+			$this->query
+				->where ('rowId', $this->params['rowId']);
+		}
 	}
-
 }
