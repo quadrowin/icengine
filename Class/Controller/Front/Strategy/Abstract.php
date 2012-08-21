582aea7228d7cdf527c673af42a642bd8e16a843
morph <morph@morph.ic>
2012-08-21 09:41:38 +0700

    -- fix

---
+++ Class/Controller/Front/Strategy/Abstract.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ice;
+
+/**
+ * @desc Абстрактная стратегрия для фронт контроллера
+ * @author Илья Колесников
+ */
+class Controller_Front_Strategy_Abstract
+{
+	/**
+	 * @desc Рабочий метод стратегии
+	 */
+	public function execute ()
+	{
+
+	}
+}
---
+++ Class/Model/Mapper/Scheme/Render/Mysql/Field/Double.php
@@ -0,0 +1,12 @@
+<?php
+
+Loader::load ('Model_Mapper_Scheme_Render_Mysql_Field');
+
+/**
+ * @desc Рендер поля типа double
+ * @author Илья Колесников
+ */
+class Model_Mapper_Scheme_Render_Mysql_Field_Double extends Model_Mapper_Scheme_Render_Mysql_Field
+{
+
+}
