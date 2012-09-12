9752ea9d7ff0c896b328a1958ca0bdfd074b2822
morph <morph@morph.ic>
2012-09-12 14:47:09 +0700

    -- fix

--- Class/Router.php
+++ Class/Router.php
@@ -1,86 +1,74 @@
 <?php
+
 /**
+ * Механизм определения роута по адресу
  *
- * @desc Механизм определения роута по адресу
- * @author Юрий Шведов
- * @package IcEngine
- *
+ * @author goorus, morph
  */
 class Router
 {
-
 	/**
+	 * Текущий роут
+	 *
 	 * @var Route
 	 */
-	private static $_route;
+	private static $route;
 
 	/**
-	 * @desc Обнулить текущий роут
+	 * Обнулить текущий роут
 	 */
-	public static function clearRoute ()
+	public static function clearRoute()
 	{
-		self::$_route = null;
+		self::$route = null;
 	}
 
 	/**
-	 * @desc Разбирает запрос и извлекат параметры согласно
+	 * Разбирает запрос и извлекат параметры согласно
+	 *
 	 * @return Route
 	 */
-	public static function getRoute ()
+	public static function getRoute()
 	{
-		if (is_null (self::$_route)) {
-			$url = Request::uri ();
-            Loader::load ('Route');
-			$url = $url ?: '/';
-			self::$_route = Route::byUrl($url);
-			if (!self::$_route) {
-				return;
-			}
-			$tempVars = array();
-			preg_match_all(
-				'#' . self::$_route['pattern'] . '#u', $url, $tempVars
-			);
-			$vars = array();
-			for ($i = 1, $count = sizeof($tempVars); $i <= $count; $i++) {
-				if (empty($tempVars[$i][0])) {
-					continue;
-				}
-				$vars[] = $tempVars[$i][0];
-			}
-			$parts = array();
-			preg_match_all(
-				'#(\:([\w\d]+)|{([\w\d]+)})#', self::$_route['route'], $parts
-			);
-			$parts = array_merge($parts[2], $parts[3]);
-			for ($i = 1, $count = sizeof($parts); $i <= $count; $i++) {
-				if (empty($parts[$i])) {
-					unset($parts[$i]);
-				}
-			}
-			$parts = array_values($parts);
-			for ($i = 0, $count = sizeof($parts); $i < $count; $i++) {
-				Request::param(
-					$parts[$i],
-					isset($vars[$i]) ? $vars[$i] : null
- 				);
-			}
-			if (isset(self::$_route->params)) {
-				foreach (self::$_route->params as $param => $value) {
-					if (Request::param($param)) {
-						continue;
-					}
-					Request::param($param, $value);
-				}
-			}
-			self::stringGet();
+		if (!is_null(self::$route)) {
+			return self::$route;
 		}
-		return self::$_route;
+		Loader::load ('Route');
+		$url = Request::uri();
+		$route = Route::byUrl($url);
+		self::$route = $route;
+		if (!self::$route) {
+			return;
+		}
+		if (!empty($route['params'])) {
+			foreach ($route['params'] as $paramName => $paramValue) {
+				Request::param($paramName, $paramValue);
+			}
+		}
+		$firstParamPos = strpos($route['route'], '{');
+		if ($firstParamPos !== false && $route['patterns']) {
+			$baseMatches = array();
+			preg_match_all($route['pattern'], $url, $baseMatches);
+			if (!empty($baseMatches[0][0])) {
+				$keys = array_keys($route['patterns']);
+				foreach ($baseMatches as $i => $data) {
+					if (!empty($data[0])) {
+						Request::param($keys[$i - 1], $data[0]);
+					} else {
+						$part = $route['patterns'][$keys[$i - 1]];
+						if (isset($part['default'])) {
+							Request::param($keys[$i - 1], $part['default']);
+						}
+					}
+				}
+			}
+		}
+		self::setParamsFromRequest();
 	}
 
 	/**
 	 * Отдать в $_REQUEST то, что прилетело из get
 	 */
-	public static function stringGet()
+	public static function setParamsFromRequest()
 	{
 		$gets = Request::stringGet();
 		if ($gets) {
