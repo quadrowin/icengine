6b68816e38b7f4ae8290dce7af49336bf1eec0ac
morph <morph@morph.ic>
2012-09-26 13:04:46 +0700

    -- fix

--- js/Controller/Admin/Attribute.js
+++ js/Controller/Admin/Attribute.js
@@ -1,20 +1,20 @@
 var Controller_Admin_Attribute = {
-	
+
 	viewFormAdd : function () {
 		$('#add_attr').show ();
 		$('#attrs #view').hide ();
 	},
-	
+
 	add : function (form) {
 		var data = Helper_Form.asArray ($(form));
-		
+
 		function callback (result) {
-			
+
 			function callbackIndex (result)
 			{
 				$('#attrs').html (result.html);
 			}
-			
+
 			Controller.call (
 				'Admin_Attribute/index',
 				{
@@ -24,15 +24,15 @@ var Controller_Admin_Attribute = {
 				callbackIndex
 			);
 		}
-		
+
 		$('#add_attr').hide ();
-		
+
 		Controller.call (
 			'Admin_Attribute/add',
 			data,
 			callback
 		);
 	}
-}
+};
 
 
