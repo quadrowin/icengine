d8beef18a20170211d4678164e7a9fadbf4379ae
neon <neon@localhost>
2012-10-13 14:50:05 +0700

    f

--- Class/Model/Option/Key.php
+++ Class/Model/Option/Key.php
@@ -1,26 +1,29 @@
 <?php
 /**
- * 
+ *
  * @desc Опция для выбора по первичному ключу
- * @param string|integer|array ['key'] 
+ * @param string|integer|array ['key']
  * @author Юрий Шведов
  * @package IcEngine
  *
  */
 class Model_Option_Key extends Model_Option
 {
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see Model_Option::before()
 	 */
 	public function before ()
 	{
-		$field = 
-			$this->collection->modelName () . 
+		$field =
+			$this->collection->modelName () .
 			'.' .
 			$this->collection->keyField ();
+		if ($this->params['field']) {
+			$field = $this->params['field'];
+		}
 		$this->query->where ($field, $this->params ['key']);
 	}
-	
+
 }
