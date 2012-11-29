8ec1f0b8453063557cebfffe0231e392ce5dbeff
neon <neon@localhost>
2012-11-29 13:29:18 +0700

    f

--- Class/Model/Component.php
+++ Class/Model/Component.php
@@ -1,35 +1,32 @@
 <?php
 
 /**
- *
- * @desc Модель компонента.
+ * Модель компонента.
  * Компоненты - модели, крепящиеся к другим моделям.
  * Коллекция компонентов может быть получена от модели через метод
  * component ().
+ *
  * @author Юрий Шведов
  * @package IcEngine.
- *
  */
 abstract class Model_Component extends Model_Child
 {
 
     /**
-     * @desc Переподключение компонента к другой модели
+     * Переподключение компонента к другой модели
+	 *
      * @param Model $model Модель, к которой будет подключен компонент.
      * @return Model_Component Этот компонент.
      */
-    public function rejoin (Model $model)
+    public function rejoin(Model $model)
     {
-    	if ($model)
-    	{
-    		$this->update (array (
-	            'table'	=> $model->table (),
-	            'rowId'	=> $model->key ()
+    	if ($model) {
+    		$this->update(array(
+	            'table'	=> $model->table(),
+	            'rowId'	=> $model->key()
 	        ));
-    	}
-    	else
-    	{
-	        $this->update (array (
+    	} else {
+	        $this->update(array(
 	            'table'	=> '',
 	            'rowId'	=> 0
 	        ));
@@ -38,12 +35,15 @@ abstract class Model_Component extends Model_Child
     }
 
 	/**
-	 * @desc Модель, к которой привязан компонент в данный момент.
+	 * Модель, к которой привязан компонент в данный момент.
+	 *
 	 * @return Model
 	 */
-    public function model ()
+    public function model()
     {
-    	return Model_Manager::byKey ($this->table, $this->rowId);
+    	return $this->getService('modelManager')->byKey(
+			$this->table,
+			$this->rowId
+		);
     }
-
 }
