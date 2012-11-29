56642e0852b8f606c86b526858405d0ef8175c3e
neon <neon@localhost>
2012-11-29 13:21:04 +0700

    f

--- Controller/Background.php
+++ Controller/Background.php
@@ -1,14 +1,13 @@
 <?php
 /**
+ * Контроллер фоновых процессов.
  *
- * @desc Контроллер фоновых процессов.
+ *
  * @author Юрий Шведов
  * @package IcEngine
- *
  */
 class Controller_Background extends Controller_Abstract
 {
-
     /**
      *
      * @var Background_Agent_Manager
@@ -19,13 +18,13 @@ class Controller_Background extends Controller_Abstract
 	 *
 	 * @return Background_Agent
 	 */
-	protected function _getAgent ()
+	protected function _getAgent()
 	{
-		$id = $this->_input->receive ('id');
-		$agent = Model_Manager::byKey ('Background_Agent', $id);
-		if (!$agent)
-		{
-			$this->_output->send (array (
+		$id = $this->_input->receive('id');
+		$modelManager = $this->getService('modelManager');
+		$agent = $modelManager->byKey('Background_Agent', $id);
+		if (!$agent) {
+			$this->_output->send(array(
 				'error'	=> 'Agent not found.'
 			));
 		}
@@ -36,26 +35,24 @@ class Controller_Background extends Controller_Abstract
 	 *
 	 * @return Background_Agent_Session
 	 */
-	protected function _getSession ()
+	protected function _getSession()
 	{
-		list (
+		list(
 			$session_id,
 			$session_key
-		) = $this->_input->receive (
+		) = $this->_input->receive(
 			'session_id',
 			'session_key'
 		);
-
-		$session = Model_Manager::byQuery (
+		$modelManager = $this->getService('modelManager');
+		$query = $this->getService('query');
+		$session = $modelManager->byQuery(
 			'Background_Agent_Session',
-			Query::instance ()
-			->where ('id', $session_id)
-			->where ('key', $session_key)
+			$query->where('id', $session_id)
+			->where('key', $session_key)
 		);
-
-		if (!$session)
-		{
-			$this->_output->send (array (
+		if (!$session) {
+			$this->_output->send(array(
 				'error'	=> 'Session not found.'
 			));
 		}
@@ -65,59 +62,51 @@ class Controller_Background extends Controller_Abstract
 	/**
 	 * @return Background_Agent_Manager
 	 */
-	protected function _manager ()
+	protected function _manager()
 	{
-	    if (!$this->_manager)
-	    {
-	        $this->_manager = new Background_Agent_Manager ();
+	    if (!$this->_manager) {
+	        $this->_manager = new Background_Agent_Manager();
 	    }
 	    return $this->_manager;
 	}
 
-	public function agents ()
+	public function agents()
 	{
-		$agents = new Background_Agent_Collection ();
+		$agents = new Background_Agent_Collection();
 		$this->_output->send (array (
 			'agents'	=> $agents
 		));
 	}
 
-	public function resetState ()
+	public function resetState()
 	{
-		$agent = $this->_getAgent ();
-		if (!$agent)
-		{
+		$agent = $this->_getAgent();
+		if (!$agent) {
 			return;
 		}
-
-		$agent->resetState ();
+		$agent->resetState();
 	}
 
 	/**
 	 * @desc Вызвать следующую итерацию работы сессии и возобновить.
 	 */
-	public function resume ()
+	public function resume()
 	{
-		$session = $this->_getSession ();
-		if (!$session)
-		{
+		$session = $this->_getSession();
+		if (!$session) {
 			return;
 		}
-
-		$session->process ();
-		$this->_manager ()->resumeSession ($session);
+		$session->process();
+		$this->_manager()->resumeSession($session);
 		die ();
 	}
 
-	public function stop ()
+	public function stop()
 	{
-		$session = $this->_getSession ();
-		if (!$session)
-		{
+		$session = $this->_getSession();
+		if (!$session) {
 			return;
 		}
-
-		$session->stop ();
+		$session->stop();
 	}
-
 }
