8ef5c53c0c6822d8e50032905fc2767e0f08bf13
neon <neon@localhost>
2012-12-05 14:59:15 +0700

    f

---
+++ Class/Service/Injector.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Объект, который внедряет зависимости
+ * 
+ * @author
+ */
+class Service_Injector
+{
+    /**
+     * Получить ижектор по имени
+     * 
+     * @param string $name
+     * @param Service_Locator $serviceLocator
+     * @return Service_Injector_Abstract
+     */
+    public function get($name, $serviceLocator = null)
+    {
+        $className = 'Service_Injector_' . $name;
+        $injector = new $className;
+        if ($serviceLocator) {
+            $injector->setServiceLocator($serviceLocator);
+        }
+        return $injector;
+    }
+}
