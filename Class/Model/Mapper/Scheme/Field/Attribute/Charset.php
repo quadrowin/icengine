febc8ca77f5686ab11f107109ae3d403d56af0ad
den85 <den85@mint85>
2012-12-10 13:16:59 +0700

    ??????????? ? ???????

---
+++ Class/Model/Mapper/Scheme/Field/Attribute/Charset.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @Атрибут поля charset для схемы связей модели
+ * @author Илья Колесников
+ */
+class Model_Mapper_Scheme_Field_Attribute_Charset extends Model_Mapper_Scheme_Field_Attribute_Abstract
+{
+
+}
