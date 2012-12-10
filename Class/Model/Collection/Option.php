9d522a2c133f243716e7f8db3abd390313b9faf8
markov <markov@markov-desktop>
2012-12-10 13:01:06 +0700

    fix

---
+++ Class/Model/Collection/Option.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *
+ * @desc Опция коллекции.
+ * @author Юрий Шведов, Илья Колесников
+ * @package IcEngine
+ *
+ */
+class Model_Collection_Option
+{
+
+	/**
+	 * @desc Название опции
+	 * @var string
+	 */
+	protected $_name;
+
+	/**
+	 * @desc Параметры
+	 * @var array
+	 */
+	protected $_params;
+
+	/**
+	 * @desc Опция
+	 * @var Model_Collection_Option_Abstract
+	 */
+	protected $_option;
+
+	/**
+	 * @desc Создает и возвращает опцию
+	 * @param string $name Название опции.
+	 * @param array $params Параметры применения.
+	 */
+	public function __construct ($name, $params = array ())
+	{
+		$this->_name = $name;
+		$this->_params = $params;
+	}
+
+	/**
+	 * @desc Наложение опции.
+	 * @param string $type "before" or "after"
+	 * @param Model_Collection $collection
+	 * @param Query_Abstract $query
+	 * @return mixed Результат наложения.
+	 */
+	public function execute ($type, $collection, Query_Abstract $query)
+	{
+        $serviceLocator = IcEngine::serviceLocator();
+        $executorService = $serviceLocator->getService('executor');
+        $collectionOptionManager = $serviceLocator->getService(
+            'collectionOptionManager'
+        );
+		if (!$this->_option)
+		{
+			$this->_option = $collectionOptionManager->get(
+				$this->_name,
+				$collection
+			);
+		}
+		return $executorService->execute(
+			array ($this->_option, $type),
+			array ($collection, $query, $this->_params)
+		);
+	}
+
+	/**
+	 * @desc Получить имя опшина
+	 * @return string
+	 */
+	public function getName ()
+	{
+		return $this->_name;
+	}
+
+	/**
+	 * @desc Возвращает параметры опции.
+	 * @return array
+	 */
+	public function getParams ()
+	{
+		return $this->_params;
+	}
+
+}
---
+++ Controller/Manager/ControllerManagerDelegeeParam.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace IcEngine\Controller\Manager;
+
+/**
+ * Делигат для передачи в транспорт контроллера параментров из метода 
+ * класса контроллера
+ * 
+ * @author morph
+ */
+class ControllerManagerDelegeeParam extends ControllerManagerDelegeeAbstract
+{
+    /**
+     * @inheritdoc 
+     * @see IcEngine\Controller\Manager\ControllerManagerDelegeeAbstract::call
+     */
+    public function call($controller, $context)
+    {
+        $reflection = new \ReflectionMethod($controller, $context->getAction());
+        $params = $reflection->getParameters();
+        $currentInput = $controller->getInput();
+        $provider = $currentInput->getProvider(0);
+        $resultParams = array();
+        if (!$params) {
+            return array();
+        }
+        foreach ($params as $param) {
+            $value = $currentInput->receive($param->name);
+            if (!$value && $param->isOptional()) {
+                $value = $param->getDefaultValue();
+            }
+            if ($provider) {
+                $provider->set($param->name, $value);
+            }
+            $resultParams[$param->name] = $value;
+        }
+        $context->setArgs(array_merge($resultParams, $context->getArgs()));
+    }
+}
