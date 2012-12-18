52a62ed990ab7be55ad6058e45fbb78f79bba27e
neon <neon@neon>
2012-12-18 11:39:06 +0700

    f

--- Class/Model/Proxy.php
+++ Class/Model/Proxy.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * @desc Проксирующая модель.
  * Используется в случаях, когда невозможно использовать класс самой модели.
  * @author Юрий
@@ -9,15 +9,31 @@
  */
 class Model_Proxy extends Model
 {
-	
+
 	/**
 	 * @desc Представляемая модель.
 	 * @var string
 	 */
 	protected $_modelName;
-	
+
+    /**
+	 * Изменяет значение поля
+     *
+	 * @param string $field Поле
+	 * @param mixed $value Значение
+	 */
+	public function __set($field, $value)
+	{
+        $fields = $this->fields;
+		if (isset($fields[$field])) {
+			$this->fields[$field] = $value;
+		} else {
+			throw new Exception ('Field unexists "' . $field . '".');
+		}
+	}
+
 	/**
-	 * 
+	 *
 	 * @param string $modelName
 	 * @param array $fields
 	 * @param boolean $autojoin
@@ -27,20 +43,22 @@ class Model_Proxy extends Model
 		$this->_modelName = $modelName;
 		parent::__construct ($fields);
 	}
-	
+
+
+
 	public function load ($key = null)
 	{
 	    return $this;
 	}
-	
+
 	public function modelName ()
 	{
 		return $this->_modelName;
 	}
-	
+
 	public function save ($hard_insert = false)
 	{
 	    return $this;
 	}
-	
+
 }
