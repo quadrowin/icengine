cb7590cefc0f249b553c88737c53a239d48816b2
morph <morph@localhost>
2012-12-18 12:03:52 +0700

    -- fix

--- Class/Model/Mapper/Scheme/Render/View.php
+++ Class/Model/Mapper/Scheme/Render/View.php
@@ -1,19 +1,23 @@
 <?php
 
 /**
- * @desc Фабрика представлений рендера схемы связей модели
- * @author Илья Колесников
+ * Фабрика представлений рендера схемы связей модели
+ * 
+ * @author morph
+ * @package Ice\Orm
+ * @Service("modelMapperSchemeRenderView")
  */
 class Model_Mapper_Scheme_Render_View
 {
 	/**
-	 * @desc Получить представление рендера схемы связей модели по имени
-	 * @param string $name
+	 * Получить представление рендера схемы связей модели по имени
+	 * 
+     * @param string $name
 	 * @return Model_Mapper_Scheme_Render_View_Abstract
 	 */
-	public static function byName ($name)
+	public function byName($name)
 	{
-		$class_name = 'Model_Mapper_Scheme_Render_View_' . $name;
-		return new $class_name;
+		$className = 'Model_Mapper_Scheme_Render_View_' . $name;
+		return new $className;
 	}
 }
