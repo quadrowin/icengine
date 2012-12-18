3f99e55af2362f47d7e5ebb78a6749569e1d3628
markov <markov@markov-desktop>
2012-12-18 09:52:26 +0700

    fix Helper_Array

---
+++ Class/Model/Mapper/Scheme/Render/Mysql/Field/Longtext.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @desc Рендер поля типа longtext
+ * @author Илья Колесников
+ */
+class Model_Mapper_Scheme_Render_Mysql_Field_Longtext extends Model_Mapper_Scheme_Render_Mysql_Field
+{
+
+}
