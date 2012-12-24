5d5acc03454581fef4e31b274ab651c1ea8dab08
neon <neon@neon>
2012-12-24 09:58:15 +0700

    ???????

--- Model/Authorization/Loginza/Token.php
+++ Model/Authorization/Loginza/Token.php
@@ -1,141 +1,63 @@
 <?php
+
 /**
- *
- * @desc Модель, содержащая информацию по ключу в логинзе.
- * @author Юрий Шведов
- * @package IcEngine
- *
+ * Модель, содержащая информацию по ключу в логинзе.
+ * 
+ * @author goorus, morph
  */
 class Authorization_Loginza_Token extends Model
 {
-
 	/**
-	 * @desc Config
-	 * @var array
+	 * @inheritdoc
 	 */
-	protected static $_config = array (
+	protected static $config = array (
 		// Адрес логинзы, где хранится результат авторизации
-		'loginza_url'				=> 'http://loginza.ru/api/authinfo?token={$token}'
+		'loginzaUrl'    => 'http://loginza.ru/api/authinfo?token={$token}'
 	);
 
 	/**
-	 * @desc Текущие данные (полученные в этом процессе).
-	 * @var Authorization_Loginza_Token
+	 * Текущие данные (полученные в этом процессе).
+	 * 
+     * @var Authorization_Loginza_Token
 	 */
-	protected static $_current;
+	protected static $current;
 
 	/**
-	 * @desc Перенаправление пользователя с ключом результата авторизации.
-	 * @param mixed $token Ключ сессии в логинзе. Если null, ключ будет
+	 * Перенаправление пользователя с ключом результата авторизации.
+	 * 
+     * @param mixed $token Ключ сессии в логинзе. Если null, ключ будет
 	 * взят из GET параметров запроса.
 	 * @return Authorization_Loginza_Token Данные, полученные от логинзы.
 	 */
-	public static function tokenData ($token = null)
+	public function tokenData($token)
 	{
-		if (is_null ($token))
-		{
-			if (self::$_current)
-			{
-				return self::$_current;
-			}
-			$token = Request::get ('lztoken');
-			if (!$token)
-			{
-				$token = Request::post ('token');
-			}
-		}
-
-		if (!$token)
-		{
+		if (!$token) {
 			return null;
 		}
-
-		self::$_current = new self (array (
-			'time'		=> Helper_Date::toUnix (),
+        if (self::$current && self::$current->token == $token) {
+            return self::$current;
+        }
+		self::$current = new self(array(
+			'time'		=> $this->getService('date')->toUnix(),
 			'token'		=> $token,
 		));
-
-		$url = str_replace (
-			'{$token}',
-			$token,
-			self::$_current->config ()->loginza_url
+		$url = str_replace(
+			'{$token}', $token, self::$current->config()->loginzaUrl
 		);
-
-		$result = file_get_contents ($url);
-		$data = json_decode ($result, true);
-
-		if (isset ($data ['error_type']))
-		{
-			// Не удалось авторизоваться
-//			$this->_output->send (array (
-//				'error'	=> $data ['error_type'],
-//				'data'	=> array (
-//					'error_type'	=> $data ['error_type'],
-//					'error_message'	=> $data ['error_message']
-//				)
-//			));
-			self::$_current = null;
+		$result = file_get_contents($url);
+		$data = json_decode($result, true);
+		if (isset($data['error_type'])) {
+			self::$current = null;
 			return null;
 		}
-
 		// Успешная авторизация
-		self::$_current->data ('data', $data);
-
-		self::$_current->set (array (
+		self::$current->data('data', $data);
+		self::$current->set(array (
 			'data'		=> $result,
-			'email'		=>
-				isset ($data ['email'])
-				? $data ['email']
-				: '',
-			'identity'	=> $data ['identity'],
-			'provider'	=>
-				isset ($data ['provider'])
-				? $data ['provider']
-				: ''
+			'email'		=> isset($data['email']) ? $data['email'] : '',
+			'identity'	=> $data['identity'],
+			'provider'	=> isset($data['provider']) ? $data['provider'] : ''
 		));
-
-		return self::$_current;
+		return self::$current;
 	}
-
-	/**
-	 * @desc Возвращает имя пользователя.
-	 * Возвращает первое из возможных значения: никнейм, имя, часть
-	 * емейла до "@".
-	 * @return string
-	 */
-	public function extractName ()
-	{
-		$data = $this->data ('data');
-
-		// Никнейм
-		if (isset ($data ['nickname']) && $data ['nickname'])
-		{
-			return
-				is_array ($data ['nickname']) ?
-					reset ($data ['nickname']) :
-					$data ['nickname'];
-		}
-
-		// Имя
-		if (isset ($data ['name']) && $data ['name'])
-		{
-			return
-				is_array ($data ['name']) ?
-					reset ($data ['name']) :
-					$data ['name'];
-		}
-
-		// емейл
-		if (isset ($data ['email']) && $data ['email'])
-		{
-			return Helper_Email::extractName (
-				is_array ($data ['email']) ?
-					reset ($data ['email']) :
-					$data ['email']
-			);
-		}
-
-		return '';
-	}
-
 }
