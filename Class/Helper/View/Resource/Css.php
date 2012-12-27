5299db9ff0839f1392bfbea9270ae4d39e963c7f
morph <morph@localhost>
2012-12-27 17:33:23 +0700

    -- fix

--- Class/Helper/View/Resource/Css.php
+++ Class/Helper/View/Resource/Css.php
@@ -11,14 +11,15 @@ class Helper_View_Resource_Css
 	 * Упаковывает
 	 *
 	 * @param string $content
+     * @param string $filename
 	 * @return string
 	 */
-	public static function pack($content)
+	public static function pack($content, $filename)
 	{
-		$content = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '',
+		$replaceContent = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '',
 			$content);
 		$content = str_replace(array ("\r", "\t", '@CHARSET "UTF-8";'), '',
-			$content);
+			$replaceContent);
 		return $content;
 	}
 }
