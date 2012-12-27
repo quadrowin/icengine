d86c8b5f2b403bc8056c0e2ada670ba11bf00211
markov <markov@markov-desktop>
2012-12-27 16:39:08 +0700

    fix

--- Controller/Authorization/Vipgeo.php
+++ Controller/Authorization/Vipgeo.php
@@ -9,6 +9,12 @@ class Controller_Authorization_Vipgeo extends Controller_Abstract
 {
 	/**
 	 * Всплывающее окошко авторизации
+     * 
+     * @Route(
+     *      "/loginza/",
+     *      "name"="authVipgeo",
+     *      "weight"=10
+     * )
 	 */
 	public function formPopup()
 	{
