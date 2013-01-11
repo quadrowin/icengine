85a246c54c5409f4d068920ea826ba9ab60d69df
morph <morph@localhost>
2013-01-11 14:36:25 +0700

    -- fix

--- Model/User/Guest.php
+++ Model/User/Guest.php
@@ -26,29 +26,4 @@ class User_Guest extends User_Cli
 			'password'	=> ''
 		)
 	);
-
-    /**
-	 * @inheritdoc
-	 */
-	public function init($sessionId = null)
-	{
-		$instance = $this->getInstance();
-        $resourceManager = $this->getService('resourceManager');
-        $provider = $this->getService('dataProviderManager')->get(
-            'user_session'
-        );
-        if (isset($_COOKIE['PHPSESSID'])) {
-            $sessionId = $_COOKIE['PHPSESSID'];
-        } else {
-            $sessionId = $this->getService('request')->sessionId();
-        }
-        $key = 'User_Session_1/0:' . $sessionId;
-        $data = $provider->get($key);
-        if ($data) {
-            $session = new User_Session($data);
-            $this->getService('session')->setCurrent($session);
-            $this->getService('user')->setCurrent($instance);
-        }
-		$resourceManager->set('Model', $instance->resourceKey(), $instance);
-	}
 }
