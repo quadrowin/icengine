d42bb4544829d23b545f083b2944efac1b6237a1
neon <neon@neon>
2013-01-19 12:15:31 +0700

    f

--- Model/User/Option/Password.php
+++ Model/User/Option/Password.php
@@ -14,17 +14,14 @@ class User_Option_Password extends Model_Option
 	{
 		$locator = IcEngine::serviceLocator();
 		$cryptManager = $locator->getService('cryptManager');
-		if ($this->params['value']) {
-			$password = $this->params['value'];
-			if (isset($this->params['type']) &&
-					$this->params['type'] == 'RSA') {
-				$rsa = $cryptManager->get('RSAW2');
-				$password = $rsa->encode($this->params['value']);
-			} else {
-				$password = md5($password);
-			}
-			$this->query
-				->where('password', $password);
-		}
+        $password = $this->params['value'];
+        if (isset($this->params['type']) &&
+                $this->params['type'] == 'RSA') {
+            $rsa = $cryptManager->get('RSAW2');
+            $password = $rsa->encode($this->params['value']);
+        } else {
+            $password = md5($password);
+        }
+        $this->query->where('password', $password);
 	}
 }
