dabcf6f605e5d39f0c98864601b36c0f8a6864b3
neon <neon@neon>
2013-01-19 12:35:44 +0700

    f

--- Model/User/Option/Email.php
+++ Model/User/Option/Email.php
@@ -12,9 +12,13 @@ class User_Option_Email extends Model_Option
      */
     public function before()
     {
+        $locator = IcEngine::serviceLocator();
+        $queryBuilder = $locator->getService('query');
         $email = $this->params['value'];
-        $this->query
-            ->where('email', $email)
-            ->orWhere('login', $email);
+        $this->query->where(
+            $queryBuilder
+                ->where('email', $email)
+                ->orWhere('login', $email)
+        );
     }
 }
