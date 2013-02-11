47ca10f1f064e0abe306c0f5a52389405d91be6d
den85 <den85@mint85>
2013-02-11 15:29:03 +0700

    --f

--- Class/Module/Manager.php
+++ Class/Module/Manager.php
@@ -22,13 +22,14 @@ class Module_Manager extends Manager_Abstract
 		if (isset(self::$_modules[$module_name])) {
 			return;
 		}
-		$module_dir = $module_name . '/';
+		$module_dir = IcEngine::root() .$module_name . '/';
+        $config_dir = $module_name . '/';
 		Loader::addPath ('Class', $module_dir . 'Class/');
 		Loader::addPath ('Class', $module_dir . 'Model/');
 		Loader::addPath ('Class', $module_dir);
 		Loader::addPath ('Controller', $module_dir . 'Controller/');
 		Loader::addPath ('includes', $module_dir . 'includes/');
-		Config_Manager::addPath ($module_dir . 'Config/');
+		Config_Manager::addPath ($config_dir . 'Config/');
 		if ($module_name != 'Ice') {
 			Config_Manager::addPath (IcEngine::root() . 'Ice/Config/Module/' .
 				$module_name . '/');
