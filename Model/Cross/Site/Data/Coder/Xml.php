aeb5eced97b30843f210af90aabc1d40aa5da3ef
morph <morph@localhost>
2013-02-17 11:33:14 +0700

    -- fix

---
+++ Model/Cross/Site/Data/Coder/Xml.php
@@ -0,0 +1,16 @@
+<?php
+
+class Cross_Site_Data_Coder_Xml extends Cross_Site_Data_Coder_Abstract
+{
+	
+	public function encode (array $message)
+	{
+		return json_encode ($message);
+	}
+	
+	public function decode ($message)
+	{
+		return json_decode ($message, true);
+	}
+	
+}
