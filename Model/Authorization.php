8611fc3da3227784bd6afdad48977c3e1f852ecb
morph <morph@localhost>
2013-02-22 10:53:46 +0700

    -- fix

--- Model/Authorization.php
+++ Model/Authorization.php
@@ -1,27 +1,51 @@
 <?php
 /**
- *  Метод авторизации пользователя.
+ * Метод авторизации пользователя.
  * Фабрика для моделей авторизации. Такое разделение позволяет
  * для каждого пользователя реализовать несколько методов авторизации
  * (по логин, по емейлу, по телефону и т.п.)
  *
- * @author Юрий Шведов
- * @package IcEngine
+ * @author goorus, morph
+ * @Orm\Entity(source="Defined")
+ * @Orm\Profile("default")
+ * @Service("authorization")
  */
-class Authorization extends Model_Factory
+class Authorization extends Model_Defined
 {
+    /**
+     * @Orm\Field\Int(Auto_Increment)
+     * @Orm\Index\Key
+     */
+    public $id;
+    
+    /**
+     * @Orm\Field\Varchar
+     */
+    public $name;
+    
+    /**
+     * @inheritdoc
+     */
+    public static $rows = array(
+        array(
+            'id'    => 1,  
+            'name'  => 'Login_Password_Sms',
+        )
+    );
+    
 	/**
 	 *  Возвращает модель авторизации по названию
 	 *
 	 * @param string $name Название модели авторизации
 	 * @return Authorization_Abstract
 	 */
-	public static function byName ($name)
+	public function byName ($name)
 	{
-		$query = $this->getService('query');
-		return $this->getService('modelManager')->byQuery(
-			__CLASS__,
-			$query->where('name', $name)
-		);
+		return $this->getService('modelManager')->byOptions(
+			'Authorization'
+		)->addOptions(array(
+            'name'  => '::Name',
+            'value' => $name
+        ));
 	}
 }
