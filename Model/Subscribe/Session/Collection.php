66a19939307785f7281910d36c4e9c55b8d862c8
morph <morph@morphdev>
2013-02-28 11:54:27 +0800

    -- fix

---
+++ Model/Subscribe/Session/Collection.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @desc Коллекция сессий рассылки
+ * @package IcEngine
+ * 
+ */
+class Subscribe_Session_Collection extends Model_Collection
+{
+	
+}
---
+++ Model/Task/Queue/Collection.php
@@ -0,0 +1,6 @@
+<?php
+
+class Task_Queue_Collection extends Model_Collection
+{
+	
+}
