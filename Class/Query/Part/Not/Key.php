f71ecda59b1e1cf23763ac9b240c94368585809b
morph <morph@morphdev>
2013-02-28 21:11:19 +0800

    -- fix

--- Class/Query/Part/Not/Key.php
+++ Class/Query/Part/Not/Key.php
@@ -11,12 +11,14 @@ class Query_Part_Not_Key extends Query_Part
 	 */
 	public function query()
 	{
+        $serviceLocator = IcEngine::serviceLocator();
+        $modelScheme = $serviceLocator->getService('modelScheme');
 		$key = $this->params['key'];
 		$modelName = $this->modelName;
         $keyField = isset($this->params['keyField']) ?
             $this->params['keyField'] : '';
         if (!$keyField) {
-            $keyField = Model_Scheme::keyField($modelName);
+            $keyField = $modelScheme->keyField($modelName);
         }
 		if (!is_array($key)) {
 			$this->query->where($modelName . '.' . $keyField . ' != ?',
