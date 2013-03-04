a29696b4d41dcb3a6fcc3738fb1675ef36cc4b65
neon <neon@neon>
2013-03-04 18:52:51 +0700

    f

--- Controller/Schedule.php
+++ Controller/Schedule.php
@@ -24,13 +24,17 @@ class Controller_Schedule extends Controller_Abstract
             ));
         $currentTs = time();
         $helperDate = $this->getService('helperDate');
+        $log = '/home/www/newvipgeo.ru/sc.txt';
+        $currentLog = file_get_contents($log);
+        file_put_contents($log, $currentLog . PHP_EOL . $currentTs);
         foreach ($schedules as $schedule) {
             $scheduleTs = $schedule['lastTs'] + $schedule['deltaSec'];
             if ($scheduleTs > $currentTs) {
                 continue;
             }
             $schedule->update(array(
-                'lastTs'    => $currentTs
+                'lastTs'    => $currentTs,
+                'lastDate'  => $helperDate->toUnix()
             ));
             echo $schedule['controllerAction'] . PHP_EOL;
             exec('./ice ' . $schedule['controllerAction']);
