332714c8e8bf696e7cea0baf49f7d71f5677072f
neon <neon@neon>
2013-03-07 14:57:47 +0700

    ???????

--- Class/Cache/Manager.php
+++ Class/Cache/Manager.php
@@ -1,52 +1,51 @@
 <?php
 
 /**
+ * Менеджер кэшей
  * 
- * @package IcEngine
- *
+ * @author goorus
+ * @Service("cacheManager")
  */
 class Cache_Manager
 {
-
     /**
-     * @desc Кэшеры.
+     * Кэшеры
+     * 
      * @var array <Data_Provider_Abstract>
      */
-    protected static $_cachers = array ();
+    protected $cachers = array();
     
     /**
-     * @desc Получить кэшеров класса
+     * Получить кэшеров класса
+     * 
      * @param string $class
      * @return Data_Provider_Abstract
      */
-    public static function cacherFor ($class)
+    public function cacherFor($class)
     {
-        if (!isset (self::$_cachers [$class]))
-        {
-            self::$_cachers [$class] = new Data_Provider_Abstract ();
+        if (is_object($class)) {
+            $class = get_class($class);
         }
-        return self::$_cachers [$class]; 
+        if (!isset($this->cachers[$class])) {
+            $this->cachers[$class] = new Data_Provider_Abstract();
+        }
+        return $this->cachers[$class]; 
     }
     
     /**
-     * @desc загрузить конфиг кэшеров
+     * Загрузить конфиг кэшеров
+     * 
      * @param string $file
      * @return Cache_Options
      */
-    public static function loadConfig ($file)
+    public function loadConfig($file)
     {
-        if (!file_exists ($file))
-        {
+        if (!file_exists($file)) {
             return null;
         }
-        
-        $options = new Cache_Options ();
-        
-        $conf = new Config_Php ($file);
-
-        $options->applyConfig ($conf);
-        
+        $options = new Cache_Options();
+        $config = new Config_Php($file);
+        $options->applyConfig($config);
         return $options;
     }
-    
 }
