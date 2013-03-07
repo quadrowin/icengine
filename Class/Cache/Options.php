fdc27a4b28f60f5358acf5fba80453938decf5ce
morph <morph@morphdev>
2013-03-07 14:37:02 +0700

    -- fix

--- Class/Cache/Options.php
+++ Class/Cache/Options.php
@@ -1,17 +1,18 @@
 <?php
 /**
+ * Опции кэширования
  * 
- * @package IcEngine
- *
+ * @author goorus
  */
 class Cache_Options 
 {
     
     /**
-     * @desc Настройки
+     * Конфигурация
+     * 
      * @var array
      */
-	protected $_config = array (
+	protected $config = array(
     	/**
     	 * Время жизни кэша.
     	 * @var integer
@@ -24,59 +25,59 @@ class Cache_Options
 	    'hits'			=> 0
 	);
 	
-	public function __construct ()
-	{
-	    
-	}
-	
 	/**
-	 * @desc Применение конфига
-	 * @param Config_Abstract $config
+	 * Применение конфига
+	 * 
+     * @param Config_Abstract $config
 	 * @return Cache_Options
 	 */
-	public function applyConfig (Config_Array $config)
+	public function applyConfig(Config_Array $config)
 	{
-		$this->_config = $config->merge ($this->_config);
+		$this->config = $config->merge($this->config);
 		return $this;
 	}
 	
 	/**
-	 * @desc Возвращает время жизни кэша.
-	 * @return integer
+	 * Возвращает время жизни кэша.
+	 * 
+     * @return integer
 	 */
-	public function getExpiration ()
+	public function getExpiration()
 	{
-		return $this->_config ['expiration'];
+		return $this->config['expiration'];
 	}
 	
 	/**
-	 * @desc Возвращает количество использований кэша.
-	 * @return integer
+	 * Возвращает количество использований кэша.
+	 * 
+     * @return integer
 	 */
-	public function getHits ()
+	public function getHits()
 	{
-	    return $this->_config ['hits'];
+	    return $this->config['hits'];
 	}
     
 	/**
-	 * @desc Задает время жизни кэша.
-	 * @param integer $value
+     * Задает время жизни кэша.
+	 * 
+     * @param integer $value
 	 * @return Cache_Options
 	 */
-	public function setExpiration ($value)
+	public function setExpiration($value)
 	{
-		$this->_config ['expiration'] = $value >= 0 ? (int) $value : 0;
+		$this->config['expiration'] = $value >= 0 ? (int) $value : 0;
 		return $this;
 	}
 	
 	/**
-	 * @desc Задает количество использований кэша.
-	 * @param integer $value
+	 * Задает количество использований кэша.
+	 * 
+     * @param integer $value
 	 * @return Cache_Options
 	 */
-	public function setHits ($value)
+	public function setHits($value)
 	{
-	    $this->_config ['hits'] = $value > 0 ? (int) $value : 0;
+	    $this->config['hits'] = $value > 0 ? (int) $value : 0;
 	    return $this; 
 	}
     
--- Controller/Annotation/Route.php
+++ Controller/Annotation/Route.php
@@ -86,12 +86,12 @@ class Controller_Annotation_Route extends Controller_Abstract
                     'actions'   => $actions,
                     'params'    => array()
                 );
-                if ($data['Route']['module']) {
-                    $theRoute['params']['module'] = $data['Route']['module'];
-                }
                 $routeIds[] = $route;
                 if ($params) {
                     $theRoute['params'] = $params;
+                }
+                if ($data['Route']['module']) {
+                    $theRoute['params']['module'] = $data['Route']['module'];
                 }
                 if ($components) {
                     $theRoute['patterns'] = $components;
