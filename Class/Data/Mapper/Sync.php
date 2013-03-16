5d37cf2fd534c8a0ea9bcb04d159a2faa8292f63
morph <morph@morphdev>
2013-03-16 13:06:15 +0700

    -- fix

--- Class/Data/Mapper/Sync.php
+++ Class/Data/Mapper/Sync.php
@@ -72,9 +72,8 @@ class Data_Mapper_Sync extends Data_Mapper_Abstract
         $filtersNames = array_keys($filters);
         if (!$filters && !$priorityFields) {
             $result = $this->staticMapper->execute($ds, $query, $options);
-        } elseif (is_array($filters) && is_array($criterias) && 
-            !array_diff($filters, $criterias) && 
-            !array_diff($filtersNames, $criteriasNames)) {
+        } elseif (!array_diff($filtersNames, $criteriasNames) &&
+            !array_diff($filters, $criterias)) {
             $result = $this->staticMapper->execute($ds, $query, $options);
         } elseif (!array_diff($criteriasNames, $priorityFields)) {
             $result = $this->staticMapper->execute($ds, $query, $options);
