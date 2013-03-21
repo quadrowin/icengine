439ce5e0db826407b383b3f89665f8fe9795eea3
morph <morph@morphdev>
2013-03-21 12:53:41 +0700

    -- fix

--- Class/Query/Abstract.php
+++ Class/Query/Abstract.php
@@ -66,13 +66,9 @@ class Query_Abstract
                 $this->parts[$partName][] = $data[$partName];
                 break;
             case Query::MERGE:
-                if ($this->parts[$partName]) {
-                    $this->parts[$partName] = array_merge(
-                        $this->parts[$partName], $data[$partName]
-                    );
-                } else {
-                    $this->parts[$partName] = $data[$partName];
-                }
+                $this->parts[$partName] = array_merge(
+                    $this->parts[$partName], $data[$partName]
+                );
                 break;
             case Query::REPLACE:
                 $this->parts[$partName] = $data[$partName];
