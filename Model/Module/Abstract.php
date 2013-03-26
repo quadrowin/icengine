5f57ce00f8895fc3d1bf590482c3ceb6dcb3c8e8
den85 <den85@mint85>
2013-03-26 14:09:53 +0700

    --f

--- Model/Module/Abstract.php
+++ Model/Module/Abstract.php
@@ -2,5 +2,13 @@
 
 class Module_Abstract extends Model_Factory_Delegate
 {
+    /**
+     * Получить путь модуля
 
+     * @return string
+     */
+    public function path()
+    {
+        return $this->name . '/';
+    }
 }
