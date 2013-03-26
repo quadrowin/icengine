3a89f6c5e08a8d029992d2587eeed3a5cf75f0ec
neon <neon@neon>
2013-03-26 14:36:43 +0700

    f

--- Class/Query/Translator/Mysql/Truncate/Table.php
+++ Class/Query/Translator/Mysql/Truncate/Table.php
@@ -2,24 +2,24 @@
 
 /**
  * Транслятор запроса типа truncate table для mysql
- * 
+ *
  * @author goorus, morph
  */
-class Query_Translator_Mysql_Truncate_Table extends 
+class Query_Translator_Mysql_Truncate_Table extends
     Query_Translator_Mysql_Alter_Table
 {
 	const SQL_TRUNCATE_TABLE = 'TRUNCATE TABLE';
 
 	/**
 	 * Рендерит часть запроса truncate table
-	 * 
+	 *
      * @param Query_Abstract $query
 	 * @return string
 	 */
 	public function doRenderTruncateTable(Query_Abstract $query)
 	{
         $modelScheme = $this->modelScheme();
-		$truncateTable = $this->part(Query::TRUNCATE_TABLE);
+		$truncateTable = $query->part(Query::TRUNCATE_TABLE);
         $helper = $this->helper();
         $table = $modelScheme->table($truncateTable[Query::NAME]);
 		return self::SQL_TRUNCATE_TABLE . ' ' .
