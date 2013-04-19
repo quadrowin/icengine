b954c970c7c30f637a2ccd2e12da604e1bdfe3ea
morph <morph@morphdev>
2013-04-19 12:00:28 +0700

    -- fix

--- Class/Crypt/RSAW2.php
+++ Class/Crypt/RSAW2.php
@@ -18,7 +18,7 @@ class Crypt_RSAW2 extends Crypt_Abstract
 	 */
 	public function __construct()
 	{
-        IcEngine::getLoader()->requireOnce('RSA2', 'Vendor');
+        IcEngine::getLoader()->load('RSA2', 'Vendor');
 		$this->_instance = new Crypt_RSA();
 		$this->keys = $this->_instance->generate_keys('9990454949', '9990450271');
 	}
