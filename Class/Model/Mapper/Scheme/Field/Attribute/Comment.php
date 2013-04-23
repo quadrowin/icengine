9d2c23ea3885c10c3d97ce253148e90508ac6769
den85 <den85@mint85>
2013-04-23 14:31:40 +0700

    --f

---
+++ Class/Model/Mapper/Scheme/Field/Attribute/Comment.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Атрибут поля comment для схемы связей модели
+ * 
+ * @author morph
+ * @package Ice\Orm
+ */
+class Model_Mapper_Scheme_Field_Attribute_Comment extends 
+    Model_Mapper_Scheme_Field_Attribute_Abstract
+{
+
+}
