92e123a5adbbb03ee75b9ef8c0496018bb2a589e
morph <morph@morphdev>
2013-04-26 17:14:49 +0700

    ???????

--- Controller/Authorization.php
+++ Controller/Authorization.php
@@ -2,20 +2,42 @@
 
 /**
  * Контроллер авторизации
- * 
+ *
  * @author morph
  */
 class Controller_Authorization extends Controller_Abstract
 {
     /**
-     * Выход
-     * 
-     * @Route("/logout/")
-     * @Context("helperHeader")
-     */
-    public function logout($context)
-    {
-        $context->userSession->getCurrent()->delete();
-        $context->helperHeader->redirect('/');
-    }
+	 * Выход
+     *
+     *  @Route(
+     *      "/logout/",
+     *      "name"="logoutPage",
+     *      "weight"=10
+     * )
+	 */
+	public function logout()
+	{
+        if ($this->getService('request')->isAjax()) {
+            $this->task->setTemplate(null);
+        }
+		$user = $this->getService('user')->getCurrent();
+		$user->logout();
+		$session = $this->getService('session')->getCurrent();
+		$session->delete();
+		$request = $this->getService('request');
+		$redirect = $this->input->receive('redirect');
+		if (!$redirect) {
+			$redirect = $request->referer();
+		}
+		$helperUri = $this->getService('helperUri');
+		$redirect = $helperUri->validRedirect(
+			$redirect ? $redirect : self::DEFAULT_REDIRECT
+		);
+		$this->output->send(array(
+			'data'  => array(
+                'redirect'	=> $redirect
+            )
+		));
+	}
 }
