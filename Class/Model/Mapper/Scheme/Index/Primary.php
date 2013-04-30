4710b55006d48757e35976c1f6c7edf83484889d
viktor <viktor@viktor-desktop>
2013-04-30 15:37:11 +0700

    fix

---
+++ Class/Model/Mapper/Scheme/Index/Primary.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Первичный ключ для схемы связей модели
+ * 
+ * @author morph
+ * @package Ice\Orm
+ */
+class Model_Mapper_Scheme_Index_Primary extends 
+    Model_Mapper_Scheme_Index_Abstract
+{
+
+}
