eab5c6e3875dfaded215234876828bf5d6d9e0af
denis <denis@denis-VirtualBox>
2013-05-01 07:45:39 +0700

    --f

--- Class/Data/Validator/Current/User/Password.php
+++ Class/Data/Validator/Current/User/Password.php
@@ -9,16 +9,20 @@
 class Data_Validator_Current_User_Password extends Data_Validator_Abstract
 {
 
-	const INVALID = 'invalid';
+    const INVALID = 'invalid';
 
-	public function validate ($data)
-	{
-		if ($data != User::getCurrent ()->password)
-		{
-			return __CLASS__ . '/' . self::INVALID;
-		}
+    public function validate($data)
+    {
+        $currentUser = IcEngine::getServiceLocator()
+            ->getService('user')
+            ->getCurrent();
+        if ($currentUser->key() <= 0
+            || $data != $currentUser->password
+        ) {
+            return __CLASS__ . '/' . self::INVALID;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
 }
