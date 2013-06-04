30e20b8d3a9098680c647d6ae317e4baff35b02c
morph <morph@localhost>
2013-06-04 18:09:05 +0700

    -- fix

--- Class/Route.php
+++ Class/Route.php
@@ -99,6 +99,9 @@ class Route extends Objective
                 $route['actions'] = (array) $route['actions'];
             }
 			$route = array_merge($emptyRoute, (array) $route);
+            if (is_string($route)) {
+                return null;
+            }
 			$pattern = '#^' . $route['route'] . '$#';
             $hostValid = true;
             $withHost = false;
