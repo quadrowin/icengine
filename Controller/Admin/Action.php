87f88f16b74178b607cf14986a4c83c816986f70
morph <morph@localhost>
2013-08-13 14:10:57 +0700

    -- fix

--- Controller/Admin/Action.php
+++ Controller/Admin/Action.php
@@ -1,7 +1,13 @@
 <?php
 
+/**
+ * Главный помошник программиста
+ */
 class Controller_Admin_Action extends Controller_Abstract
 {
+    /**
+     * @Template(null)
+     */
     public function index($table, $rowId, $context)
     {
         $query = $context->queryBuilder
