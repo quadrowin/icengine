ab88c25f9961203a0be0e6cc5e8ba4c762b9602d
dp <dp@ds.local>
2013-08-21 13:26:26 +0700

    ??????? ?????????? ?? ????? ?????? ??????

--- Class/Controller/Action.php
+++ Class/Controller/Action.php
@@ -1,29 +1,1 @@
-<?php
-
-/**
- * Действие контроллера
- *
- * @author goorus, morph
- */
-class Controller_Action extends Objective
-{
-	/**
-	 * Флаг игнорирования
-	 */
-	const IGNORE_FLAG = 'ignore';
-
-	/**
-	 * Транспорт входной
-     *
-	 * @var Data_Transport
-	 */
-	public $input;
-
-	/**
-	 * Транспорт выходной
-     *
-	 * @var Data_Transport
-	 */
-	public $output;
-
-}
+<?php/** * Действие контроллера * * @author goorus, morph */class Controller_Action extends Objective{	/**	 * Флаг игнорирования	 */	const IGNORE_FLAG = 'ignore';	/**	 * Транспорт входной     *	 * @var Data_Transport	 */	public $input;	/**	 * Транспорт выходной     *	 * @var Data_Transport	 */	public $output;}
