7861c1e140734da12826f660441b9ee83e0fea91
morph <morph@localhost>
2013-08-26 17:21:41 +0700

    -- fix

--- Class/Helper/Model/Table.php
+++ Class/Helper/Model/Table.php
@@ -9,6 +9,13 @@
 class Helper_Model_Table extends Helper_Abstract
 {
     /**
+     * Существующие таблицы
+     * 
+     * @var array
+     */
+    protected static $tables = array();
+    
+    /**
      * Создать таблицу для подели
      *
      * @param string $modelName
@@ -33,5 +40,39 @@ class Helper_Model_Table extends Helper_Abstract
             $query = $view->render($modelName);
             $dataSource->execute($query);
         }
+    }
+    
+    /**
+     * Проверить таблицу на существование
+     * 
+     * @param string $className
+     * @return boolean
+     */
+    public function exists($className)
+    {
+        if (!self::$tables) {
+            self::$tables = $this->getTables();
+        }
+        return isset(self::$tables[$className]);
+    }
+    
+    /**
+     * Получить таблицы
+     * 
+     * @return array
+     */
+    public function getTables()
+    {
+        $dds = $this->getService('dds');
+        $queryBuilder = $this->getService('queryBuilder');
+        $modelScheme = $this->getService('modelScheme');
+        $query = $queryBuilder
+            ->show('TABLES');
+        $tables = $dds->execute($query)->getResult()->asColumn();
+        $result = array();
+        foreach ($tables as $table) {
+            $result[$modelScheme->tableToModel($table)] = true;
+        }
+        return $result;
     }
 }
