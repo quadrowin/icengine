ce8145e430c6dacff34512a02351006964c6a105
markov <markov@markov-desktop>
2013-09-09 12:34:33 +0700

    fix ????? ????????? ??????? ??? helper

--- Class/Helper/Abstract.php
+++ Class/Helper/Abstract.php
@@ -14,6 +14,29 @@ class Helper_Abstract
     protected static $serviceLocator = null;
 
     /**
+     * Конфиг контроллера
+     *
+     * @var array
+     */
+    protected $config = array();
+    
+    /**
+     * Загружает и возвращает конфиг для контроллера
+     *
+     * @return Objective
+     */
+    public function config()
+    {
+        if (is_array($this->config)) {
+            $configManager = $this->getService('configManager');
+            $this->config = $configManager->get(
+                get_class($this), $this->config
+            );
+        }
+        return $this->config;
+    }
+    
+    /**
      * @param string $name
      * @return mixed
      */
