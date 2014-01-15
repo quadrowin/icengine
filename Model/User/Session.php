1bc0fbd6a38b6fee78c5f2cdbbe70db670db9282
????? ??????? <????? ???????@localhost>
2014-01-15 12:46:12 +0700

    gdekvartira_patch_1.34_pre_release (new cabinet)

--- Model/User/Session.php
+++ Model/User/Session.php
@@ -1,59 +1,1 @@
-<?php
-
-/**
- * Сессия пользователя
- * 
- * @author neon
- * @Orm\Entity(
- *      source="user_session", 
- *      keyGen="Helper_Unique::forModel",
- *      indexes={
- *           {"phpSessionId"},
- *           {"User__id"}
- *      }
- * )
- * @Service("userSession", disableConstruct=true)
- */
-class User_Session extends User_Session_Abstract
-{
-    /**
-     * @Orm\Field\Varchar(Size=64, Not_Null)
-     * @Orm\Index\Primary
-     */
-    public $id;
-    
-    /**
-     * @Orm\Field\Varchar(Size=64, Not_Null)
-     * @Orm\Index\Key
-     */
-    public $phpSessionId;
-    
-    /**
-     * @Orm\Field\Int(Size=11, Not_Null)
-     * @Orm\Index\Key
-     */
-    public $User__id;
-    
-    /**
-     * @Orm\Field\Datetime(Not_Null)
-     */
-    public $lastActive;
-    
-    /**
-     * @Orm\Field\Varchar(Size=32, Not_Null)
-     */
-    public $remoteIp;
-    
-    /**
-     * @Orm\Field\Varchar(Size=64, Not_Null)
-     */
-    public $userAgent;
-    
-    /**
-     * @inheritdoc
-     */
-    public function getParams()
-    {
-        return array();
-    }
-}
+<?php/** * Сессия пользователя *  * @author neon * @Orm\Entity( *      source="user_session",  *      keyGen="Helper_Unique::forModel", *      indexes={ *           {"phpSessionId"}, *           {"User__id"} *      } * ) * @Service("userSession", disableConstruct=true) */class User_Session extends User_Session_Abstract{    /**     * @Orm\Field\Varchar(Size=64, Not_Null)     * @Orm\Index\Primary     */    public $id;        /**     * @Orm\Field\Varchar(Size=64, Not_Null)     * @Orm\Index\Key     */    public $phpSessionId;        /**     * @Orm\Field\Int(Size=11, Not_Null)     * @Orm\Index\Key     */    public $User__id;        /**     * @Orm\Field\Datetime(Not_Null)     */    public $lastActive;        /**     * @Orm\Field\Varchar(Size=32, Not_Null)     */    public $remoteIp;        /**     * @Orm\Field\Varchar(Size=64, Not_Null)     */    public $userAgent;    /**     * @Orm\Field\Varchar(Size=255, Not_Null)     */    public $jsonData;    /**     * @inheritdoc     */    public function getParams()    {        return array();    }}
